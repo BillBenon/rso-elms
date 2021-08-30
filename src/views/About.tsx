6193ca92e1dc0c2c32a699a19cc8ec931bfc6137
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-08-30 15:29:36 +0200

    fix some errors

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -29,7 +29,7 @@ const About = () => {
                     className="text-indigo-600 underline hover:text-indigo-500">
                     Tailwind UI
                   </a>{' '}
-                  but, you don't need it.
+                  but, you don&apos;t need it.
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
@@ -41,7 +41,7 @@ const About = () => {
                   </div>
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <a
-                      href="#"
+                      href="/"
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:ring focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
                       Live demo
                     </a>
