cad488487261ce97c801f2f841ea1124875a054d
IrakozeLoraine <mukezwa@gmail.com>
2021-08-31 13:44:09 +0200

    build(tailwind): color variatios

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,18 @@ module.exports = {
       'primary-hoover': '#4F5FD6',
       secondary: '#FAFBFC',
       main: '#FFFFFF',
-      error: '#EE4040',
-      warning: '#FACD23',
-      succes: '#3CD278',
+      error: {
+        400: '#FDECEC',
+        500: '#EE4040',
+      },
+      warning: {
+        400: '#FEF5D3',
+        500: '#FACD23',
+      },
+      succes: {
+        400: '#E9F6F2',
+        500: '#3CD278',
+      },
       'txt-primary': '#050825',
       'txt-secondary': '#97A0A8',
     },
