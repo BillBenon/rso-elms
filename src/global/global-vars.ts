4b5733decea9e47a10f43cf04f1c82ed9f2a8026
Hirwa Blessing <hirwablessing7@gmail.com>
2021-08-31 16:59:00 +0200

    Fix: Redefine types

---
+++ src/global/global-vars.ts
@@ -0,0 +1,15 @@
+import { fontSizeStyleType, fontWeightStyleType } from '../types';
+
+export const fontWeightStyle: fontWeightStyleType = {
+  normal: 'font-normal',
+  medium: 'font-medium',
+  bold: 'font-bold',
+};
+
+export const fontSizeStyle: fontSizeStyleType = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  tiny: 'text-tiny',
+  lg: 'text-lg',
+  '2xl': 'text-2xl',
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,12 @@ export type Color =
 export type fontSize = 'xs' | 'sm' | 'tiny' | 'lg' | '2xl';
 export type width = 'default' | 'full';
 export type fontWeight = 'normal' | 'medium' | 'bold';
+export type fontWeightStyleType = {
+  // eslint-disable-next-line no-unused-vars
+  [index in fontWeight]: string;
+};
+
+export type fontSizeStyleType = {
+  // eslint-disable-next-line no-unused-vars
+  [index in fontSize]: string;
+};
