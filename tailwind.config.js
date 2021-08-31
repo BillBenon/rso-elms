694521a6ded6ddf9fd782baaa3237a475cdb5628
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-08-31 10:05:43 +0200

    feat(tailwind): color schemes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,19 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      primary: '#2337CC',
+      'primary-active': '#1C2CA3',
+      'primary-hoover': '#4F5FD6',
+      secondary: '#FAFBFC',
+      main: '#FFFFFF',
+      error: '#EE4040',
+      warning: '#FACD23',
+      succes: '#3CD278',
+      'txt-primary': '#050825',
+      'txt-secondary': '#97A0A8',
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
