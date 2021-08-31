4ff170fc52407f1ac989e74817c438069cb4c61d
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-08-31 17:14:34 +0200

    chore: color style type

--- src/global/global-vars.ts
+++ src/global/global-vars.ts
@@ -1,4 +1,4 @@
-import { fontSizeStyleType, fontWeightStyleType } from '../types';
+import { colorStyleType, fontSizeStyleType, fontWeightStyleType } from '../types';
 
 export const fontWeightStyle: fontWeightStyleType = {
   normal: 'font-normal',
@@ -12,4 +12,26 @@ export const fontSizeStyle: fontSizeStyleType = {
   tiny: 'text-tiny',
   lg: 'text-lg',
   '2xl': 'text-2xl',
+};
+
+export const bgStyle: colorStyleType = {
+  primary: 'bg-primary-500 active:bg-primary-600 hover:bg-primary-400',
+  secondary: 'bg-secondary',
+  main: 'bg-main',
+  error: 'bg-error',
+  warning: 'bg-warning',
+  success: 'bg-succes',
+  'txt-primary': 'bg-txt-primary',
+  'txt-secondary': 'bg-txt-primary',
+};
+
+export const colorStyle: colorStyleType = {
+  primary: 'text-primary-500 active:text-primary-600 hover:text-primary-400',
+  secondary: 'text-secondary',
+  main: 'text-main',
+  error: 'text-error',
+  warning: 'text-warning',
+  success: 'text-succes',
+  'txt-primary': 'text-txt-primary',
+  'txt-secondary': 'text-txt-primary',
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,10 @@
 export type Color =
-  | 'primary '
-  | 'secondary '
-  | 'main '
-  | 'error '
-  | 'warning '
-  | 'success '
+  | 'primary'
+  | 'secondary'
+  | 'main'
+  | 'error'
+  | 'warning'
+  | 'success'
   | 'txt-primary'
   | 'txt-secondary';
 
@@ -12,11 +12,11 @@ export type fontSize = 'xs' | 'sm' | 'tiny' | 'lg' | '2xl';
 export type width = 'default' | 'full';
 export type fontWeight = 'normal' | 'medium' | 'bold';
 
-export interface Link {
-  icon?: string;
-  title: string;
-  to: string;
-}
+export type colorStyleType = {
+  // eslint-disable-next-line no-unused-vars
+  [index in Color]: string;
+};
+
 export type fontWeightStyleType = {
   // eslint-disable-next-line no-unused-vars
   [index in fontWeight]: string;
@@ -26,3 +26,9 @@ export type fontSizeStyleType = {
   // eslint-disable-next-line no-unused-vars
   [index in fontSize]: string;
 };
+
+export interface Link {
+  icon?: string;
+  title: string;
+  to: string;
+}
