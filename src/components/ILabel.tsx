03d7e0249efafc11574667e9bb75cfb00e78f8ea
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-08-31 17:03:24 +0200

    chore: button

--- src/components/ILabel.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ILabel() {
-  return (
-    <div>
-      <span>text here</span>
-    </div>
-  );
-}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,19 @@
 export type Color =
-  | 'primary'
-  | 'secondary'
-  | 'primary-hover'
-  | 'main'
+  | 'primary '
+  | 'secondary '
+  | 'main '
+  | 'error '
+  | 'warning '
+  | 'success '
   | 'txt-primary'
   | 'txt-secondary';
 
 export type fontSize = 'xs' | 'sm' | 'tiny' | 'lg' | '2xl';
 export type width = 'default' | 'full';
 export type fontWeight = 'normal' | 'medium' | 'bold';
+
+export interface Link {
+  icon?: string;
+  title: string;
+  to: string;
+}
