6940c08ec1d89e947e1e2965c4d1f98f98ffd8eb
IrakozeLoraine <mukezwa@gmail.com>
2021-09-01 17:23:26 +0200

    refactor(Heading): change title prop to children node

--- src/components/Atoms/Texts/Heading.tsx
+++ src/components/Atoms/Texts/Heading.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { fontSize, fontWeight } from '../../../types';
 
 type HeadingProps = {
   fontSize: fontSize;
   fontWeight: fontWeight;
-  title: string;
+  children: ReactNode;
 };
 
-const Heading = ({ fontSize, fontWeight, title }: HeadingProps) => {
-  return <p className={`mt-5 text-${fontSize} font-${fontWeight}`}>{title}</p>;
+const Heading = ({ fontSize, fontWeight, children }: HeadingProps) => {
+  return <p className={`mt-5 text-${fontSize} font-${fontWeight}`}>{children}</p>;
 };
 
 export default Heading;
