ff8302dfce8a598198e5dd0a94e8f4262a22ff83
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-09-02 12:01:00 +0200

    feat: radio basics

--- src/components/Atoms/Input/Radio.tsx
+++ src/components/Atoms/Input/Radio.tsx
@@ -1,14 +1,42 @@
-import React, { ReactNode } from 'react';
+import React, { useState } from 'react';
+
+import { SelectData } from '../../../types';
 
 type PropType = {
-  children: ReactNode;
+  list: SelectData[];
 };
 
-export default function Radio({ children }: PropType) {
+export default function Radio({ list }: PropType) {
+  const [active, setActive] = useState('');
+
+  function handleClick(value: string) {
+    setActive(value);
+  }
+
+  function handleKeyPress(value: string) {
+    setActive(value);
+  }
+
   return (
-    <div>
-      <span className=""></span>
-      <span>{children}</span>
+    <div className="flex" role="radiogroup">
+      {list.map(({ label, value }) => (
+        <div
+          role="radio"
+          tabIndex={0}
+          key={value}
+          aria-checked={active === value}
+          aria-labelledby={label}
+          className="flex cursor-pointer justify-center items-center"
+          onClick={() => handleClick(value)}
+          onKeyPress={() => handleKeyPress(value)}>
+          <span
+            className={`mx-2 w-5 h-5 rounded-full border-solid 
+            ${
+              active === value ? 'border-6 border-primary-500' : 'border-4 border-bcolor'
+            }`}></span>
+          <span>{label}</span>
+        </div>
+      ))}
     </div>
   );
 }
