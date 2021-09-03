8080ca63b405f866490240770a0aabf64fdae5b4
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-09-03 10:24:46 +0200

    feat: cleaning app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import React, { useState } from 'react';
-
-import Icon from './components/Atoms/custom/Icon';
-import Input from './components/Atoms/Inputs/Input';
-import ILabel from './components/Atoms/Texts/ILabel';
+import React from 'react';
 
 const App = () => {
-  const [name, setName] = useState('');
-
-  return (
-    <div className="p-8 flex flex-col gap-3">
-      <ILabel color="error" title="First name" weight="bold" size="xs" />
-      <Input
-        placeholder="First"
-        fcolor="error"
-        type="text"
-        value={name}
-        handleChange={(e) => setName(e.target.value)}
-      />
-
-      {/* <Icon name="notification" /> */}
-    </div>
-  );
+  return <div className="p-8 flex flex-col gap-3"></div>;
 };
 
 export default App;
