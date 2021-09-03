16d87f28ec6256a78941abde74e5fc4b0e2f8781
Hirwa Blessing <hirwablessing7@gmail.com>
2021-09-03 13:48:11 +0200

    Change card border radius

--- src/components/Molecules/AcademyCardMolecule.tsx
+++ src/components/Molecules/AcademyCardMolecule.tsx
@@ -6,7 +6,7 @@ type ICard = { children: React.ReactNode };
 
 export default function AcademyCart({ children }: ICard) {
   return (
-    <div className="flex items-center gap-4 bg-secondary h-16 rounded-lg w-48 max-w-48 p-4">
+    <div className="flex items-center gap-4 bg-secondary h-16 rounded-md w-48 max-w-48 p-4">
       <Avatar
         image="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
         size="10"
