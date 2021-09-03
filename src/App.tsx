b1008e990020b3bd9a0ecb2f103c92758bd3dcb6
Hirwa Blessing <hirwablessing7@gmail.com>
2021-09-03 11:11:09 +0200

    Finish textarea component

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,26 @@ import ILabel from './components/Atoms/Texts/ILabel';
 
 const App = () => {
   const [name, setName] = useState('');
+  const [names, setNames] = useState('');
 
   return (
     <div className="p-8 flex flex-col gap-3">
       <ILabel color="error" title="First name" weight="bold" size="xs" />
       <Input
+        placeholder="This is a text area"
+        fcolor="warning"
+        type="text"
+        category="text_area"
+        value={names}
+        handleChange={(e: any) => setNames(e.target.value)}
+      />
+
+      <Input
         placeholder="First"
         fcolor="error"
         type="text"
         value={name}
-        handleChange={(e) => setName(e.target.value)}
+        handleChange={(e: any) => setName(e.target.value)}
       />
 
       <Icon name="notification" color="error" bgColor="error" size={16} />
