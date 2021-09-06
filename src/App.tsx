e5e59d65682d4dd2140e06c0bada8b6c113ff961
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-09-06 12:52:13 +0200

    feat: academy molecule

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import AcademyMolecule from './components/Molecules/cards/AcademyMolecule';
-
 const App = () => {
-  return (
-    <div className="p-8 flex flex-col gap-3">
-      <AcademyMolecule />
-    </div>
-  );
+  return <div className="p-8 flex flex-col gap-3"></div>;
 };
 
 export default App;
--- src/components/Molecules/cards/AcademyMolecule.tsx
+++ src/components/Molecules/cards/AcademyMolecule.tsx
@@ -5,18 +5,22 @@ import React from 'react';
 import Avatar from '../../Atoms/custom/Avatar';
 import Heading from '../../Atoms/Text/Heading';
 
-export default function AcademyMolecule() {
+type PropType = {
+  active?: boolean;
+  data: any;
+};
+
+export default function AcademyMolecule({ active = false, data }: PropType) {
   return (
-    <div id="academy-molecule">
-      <Avatar
-        src="https://www.mod.gov.rw/fileadmin/_processed_/9/c/csm_RDF_LOGO_0fa751f824.jpg"
-        alt="org avatar"
-      />
+    <div
+      id="academy-molecule"
+      className={`bg-main p-6 rounded-lg ${
+        active && 'border-4 border-solid border-primary-500'
+      }`}>
+      <Avatar src={data.image} alt={`${data.name} logo`} />
       <div>
-        <Heading>RMA Gako</Heading>
-        <p className="text-txt-secondary">
-          Rwanda Military Academy To train and educate cadets.
-        </p>
+        <Heading>{data.name}</Heading>
+        <p className="text-txt-secondary h-28">{data.description}</p>
       </div>
     </div>
   );
