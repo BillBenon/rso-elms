8b4ec0f4e675ce6a1d17837593347a43baf64003
IrakozeLoraine <mukezwa@gmail.com>
2021-09-09 12:05:21 +0200

    refactor(table): make changes on table styles

--- src/components/Atoms/custom/Row.tsx
+++ src/components/Atoms/custom/Row.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 
+import { statusStyleType } from '../../../types';
 import Badge from './Badge';
 
 type RowProps = {
   keys: string[];
-  data: any;
-  isEven: boolean;
+  data: {};
+  statusColumn?: string;
 };
 
-const Row = ({ keys, data, isEven }: RowProps) => {
+const statusColors: statusStyleType = {
+  pending: 'warning',
+  ongoing: 'warning',
+  complete: 'success',
+  active: 'success',
+  inactive: 'error',
+  suspended: 'error',
+  cancelled: 'error',
+};
+
+const Row = ({ keys, data, statusColumn }: RowProps) => {
   return (
     <>
       {keys.map((key) => (
         <td
-          className={`px-4 py-4 ${key.toLowerCase() == 'status' ? 'text-xs' : ''}`}
-          style={{ backgroundColor: `${isEven ? '#FBFAFB' : ''}` }}
+          className={`px-4 py-2 ${key.toLowerCase() === statusColumn ? 'text-xs' : ''}`}
           key={key}>
-          {key.toLowerCase() == 'status' ? (
-            <Badge badgecolor="success">{data[key]}</Badge>
+          {key.toLowerCase() === statusColumn ? (
+            <Badge badgecolor={statusColors[data[key].toLowerCase()]}>{data[key]}</Badge>
           ) : (
             data[key]
           )}
--- src/components/Molecules/Table.tsx
+++ src/components/Molecules/Table.tsx
@@ -1,18 +1,22 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 
+import '../../styles/components/molecules/table/table.scss';
+
 import React, { useState } from 'react';
 
+import Icon from '../atoms/custom/Icon';
 import Row from '../atoms/custom/Row';
 import Pagination from './Pagination';
 
 type TableProps = {
   data: {}[];
   hasAction: boolean;
+  statusColumn?: string;
 };
 
-const Table = ({ data, hasAction }: TableProps) => {
-  const [rowsPerPage] = useState(10);
+const Table = ({ data, hasAction, statusColumn }: TableProps) => {
+  const [rowsPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
 
   //Get current rows
@@ -27,7 +31,7 @@ const Table = ({ data, hasAction }: TableProps) => {
   const getHeader = () => {
     let keys = getKeys();
     return keys.map((key) => (
-      <th className="px-4 py-2" key={key}>
+      <th className="px-4 py-5 capitalize" key={key}>
         {key}
       </th>
     ));
@@ -36,26 +40,12 @@ const Table = ({ data, hasAction }: TableProps) => {
   const getRowsData = () => {
     let keys = getKeys();
     return currentRows.map((row, index) => (
-      <tr className="hover:bg-silver-400" key={index}>
-        <Row key={index} data={row} keys={keys} isEven={index % 2 === 0} />
+      <tr key={index}>
+        <Row key={index} data={row} keys={keys} statusColumn={statusColumn} />
         {hasAction ? (
-          <td
-            className="flex space-x-6 py-4"
-            style={{ backgroundColor: `${index % 2 === 0 ? '#FBFAFB' : ''}` }}>
+          <td className="flex space-x-6 py-4">
             <span onClick={() => console.log('more')}>
-              <svg
-                className="justify-center m-4 cursor-pointer"
-                width="14"
-                height="4"
-                viewBox="0 0 14 4"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <g opacity="0.4">
-                  <circle cx="2.19796" cy="1.80139" r="1.38611" fill="#222222" />
-                  <circle cx="11.9013" cy="1.80115" r="1.38611" fill="#222222" />
-                  <circle cx="7.04991" cy="1.80115" r="1.38611" fill="#222222" />
-                </g>
-              </svg>
+              <Icon name="more" stroke="txt-secondary" fill="primary" />
             </span>
           </td>
         ) : (
@@ -66,45 +56,22 @@ const Table = ({ data, hasAction }: TableProps) => {
   };
 
   return (
-    <div>
-      <div className="overflow-x-auto m-5 rounded-lg shadow-lg text-sm">
-        <table className="table-auto border-collapse w-full font-semibold">
-          <thead>
-            <tr className="text-left text-txt-secondary border-b border-silver-500">
-              {getHeader()}
-              {hasAction ? <th className="px-4 py-2 ">Actions</th> : ''}
-            </tr>
-          </thead>
-          <tbody>
-            {getRowsData()}
-            {/* <tr className="hover:bg-silver-400 py-10">
-              <td className="px-4 py-4">
-                <div>Adam</div>
-                <div className="text-txt-secondary">hello@gmail.com</div>
-              </td>
-              <td className="px-4 py-4">Permission name</td>
-              <td className="px-4 py-4 text-xs">
-                <Badge badgecolor="success">Active</Badge>
-              </td>
-              <td className="px-4 py-4">Location</td>
-            </tr> */}
-          </tbody>
-        </table>
-      </div>
-
+    <div className="overflow-x-auto rounded-lg text-sm">
+      <table className="table-auto border-collapse font-semibold bg-main w-full m-auto">
+        <thead>
+          <tr className="text-left text-txt-secondary border-b border-silver-500">
+            {getHeader()}
+            {hasAction ? <th className="px-4 py-2 ">Actions</th> : ''}
+          </tr>
+        </thead>
+        <tbody>{getRowsData()}</tbody>
+      </table>
       <Pagination
         rowsPerPage={rowsPerPage}
         totalRows={data.length}
         paginate={paginate}
         currentPage={currentPage}
       />
-
-      {/* <Paginator
-        itemsCount={totalCount}
-        pageSize={pageSize}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      /> */}
     </div>
   );
 };
