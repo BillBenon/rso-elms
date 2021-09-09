b246681d1efaf62cdd0f7245aa1af4646f12593f
IrakozeLoraine <mukezwa@gmail.com>
2021-09-09 12:03:47 +0200

    refactor(table): make changes to the table status

--- src/components/Molecules/Pagination.tsx
+++ src/components/Molecules/Pagination.tsx
@@ -5,8 +5,7 @@ import Indicator from '../atoms/custom/Indicator';
 type PaginationProps = {
   rowsPerPage: number;
   totalRows: number;
-  // eslint-disable-next-line no-unused-vars
-  paginate: (pnber: number) => void;
+  paginate: (_pnber: number) => void;
   currentPage: number;
 };
 export default function Pagination({
@@ -23,20 +22,9 @@ export default function Pagination({
 
   return (
     <div className="py-2">
-      <div>
-        <p className="text-sm text-txt-secondary text-center mb-3 font-semibold">
-          Showing
-          <span> {currentPage * rowsPerPage - 10} </span>
-          to
-          <span> {currentPage * rowsPerPage} </span>
-          of
-          <span> {totalRows} </span>
-          results
-        </p>
-      </div>
-      <nav className="block">
+      <nav className="my-2 block">
         <ul className="flex pl-0 rounded list-none flex-wrap justify-center">
-          <li className="space-x-2">
+          <li className="space-x-4">
             {pageNumbers.map((number) => (
               <Indicator
                 key={number}
@@ -48,19 +36,6 @@ export default function Pagination({
                 clicked={() => paginate(number)}>
                 {number}
               </Indicator>
-              //   <a
-              //     key={number}
-              //     onClick={() => {
-              //       paginate(number);
-              //     }}
-              //     href="#"
-              //     className={
-              //       currentPage === number
-              //         ? 'bg-blue border-red-300 text-red-500 hover:bg-blue-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium'
-              //         : 'bg-white border-gray-300 text-gray-500 hover:bg-blue-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium'
-              //     }>
-              //     {number}
-              //   </a>
             ))}
           </li>
         </ul>
