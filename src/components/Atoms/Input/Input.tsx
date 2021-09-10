1baa82b25843534c19adf6f449d51d1c164055ed
blessinghirwa <hirwablessing7@gmail.com>
2021-09-10 15:09:37 +0200

    Fix: Input event value event handling and states updates

--- src/components/Atoms/Input/Input.tsx
+++ src/components/Atoms/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { colorStyle } from '../../../global/global-vars';
 import { Color, ValueType } from '../../../types';
@@ -28,18 +28,24 @@ export default function Input({
   width = '80',
   handleChange,
 }: IProps) {
+  const [_value, _setValue] = useState(value);
+
+  function handleOnChange(e: any) {
+    _setValue(e.target.value);
+    handleChange({ name, value: e.target.value, event: e });
+  }
   return (
     <input
       placeholder={placeholder}
       name={name}
       type={type}
-      value={value}
+      value={_value}
       readOnly={readonly}
       className={`bg-transparent h-12 px-5 rounded-md ${
         full ? 'w-full' : `w-${width}`
       } focus:outline-none border-${bcolor} focus:border-${colorStyle[fcolor]} border-2`}
       /* @ts-ignore */
-      onChange={(event) => handleChange({ name, value, event })}
+      onChange={handleOnChange}
     />
   );
 }
