d7805817c99ed01b475e7a49098a3f9cf12b08cf
IrakozeLoraine <mukezwa@gmail.com>
2021-09-13 18:04:17 +0200

    fix: atom import on dropdown

--- src/components/Organisms/signup/NationalDocument.tsx
+++ src/components/Organisms/signup/NationalDocument.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import DropDown from '../../../styles/components/atoms/custom/Dropdown';
 import { ValueType } from '../../../types';
 import { validation } from '../../../utils/validations';
+import DropDown from '../../Atoms/Input/Dropdown';
 import InputMolecule from '../../Molecules/input/InputMolecule';
 
 const NationalDocuments = () => {
