25cbc135ca8eeb29e49336c398c6edaba499ad48
claranceliberi <liberintwari@gmail.com>
2021-09-13 10:12:45 +0200

    fix: able to support dynamic color

--- src/components/Atoms/Text/Heading.tsx
+++ src/components/Atoms/Text/Heading.tsx
@@ -1,15 +1,25 @@
 import React, { ReactNode } from 'react';
 
-import { fontSize, fontWeight } from '../../../types';
+import { Color, fontSize, fontWeight } from '../../../types';
 
 type HeadingProps = {
   fontSize?: fontSize;
   fontWeight?: fontWeight;
+  color?: Color;
   children: ReactNode;
 };
 
-const Heading = ({ fontSize = 'lg', fontWeight = 'medium', children }: HeadingProps) => {
-  return <h2 className={`mt-5 text-${fontSize} font-${fontWeight}`}>{children}</h2>;
+const Heading = ({
+  fontSize = 'lg',
+  fontWeight = 'medium',
+  color = 'txt-primary',
+  children,
+}: HeadingProps) => {
+  return (
+    <h2 className={`mt-5 text-${fontSize} font-${fontWeight} text-${color}`}>
+      {children}
+    </h2>
+  );
 };
 
 export default Heading;
