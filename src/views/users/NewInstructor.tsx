a9c7ac3e46e36698dfe1f5a862897dd56a6e71c0
jacques-twizeyimana <sandbergjacques500@gmail.com>
2021-09-14 16:39:48 +0200

    fix: remove unsed import

--- src/views/users/NewInstructor.tsx
+++ src/views/users/NewInstructor.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import NewInstructor from '../../components/Organisms/user/NewInstructor';
-import NewStudent from '../../components/Organisms/user/NewStudent';
 import Dashboard from '../../layout/Dashboard';
 
 export default function NewInstructorView() {
