36cb91893c3a888a1753e25956c9d978a0068070
blessinghirwa <hirwablessing7@gmail.com>
2021-09-14 16:05:33 +0200

    refactor: academy profile card component

--- src/components/Molecules/AcademyProfileCard.tsx
+++ src/components/Molecules/AcademyProfileCard.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import Avatar from '../Atoms/custom/Avatar';
+import Badge from '../Atoms/custom/Badge';
 import ILabel from '../Atoms/Text/ILabel';
 
 type ICard = { children: React.ReactNode };
 
 export default function AcademyProfileCard({ children }: ICard) {
   return (
-    <div
-      className="flex items-center gap-4 bg-secondary h-16 rounded-md w-full p-4"
-      style={{ maxWidth: '12rem' }}>
+    <Badge className="flex items-center gap-4 bg-secondary h-16 w-full p-4">
       <Avatar
         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
         size="32"
@@ -18,6 +17,6 @@ export default function AcademyProfileCard({ children }: ICard) {
       <ILabel size="xs" weight="medium">
         {children}
       </ILabel>
-    </div>
+    </Badge>
   );
 }
--- src/components/Molecules/sidebar/Sidebar.tsx
+++ src/components/Molecules/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import AcademyProfileCard from '../AcademyProfileCard';
 export default function Sidebar({ links, activeIndex }: sidebarLinksProps) {
   return (
     <div className="bg-white md:h-screen">
-      <div className="px-8 py-4">
+      <div className="px-4 py-4">
         <AcademyProfileCard>University of Rwanda</AcademyProfileCard>
       </div>
       <SidebarLinks links={links} activeIndex={activeIndex || 0} />
