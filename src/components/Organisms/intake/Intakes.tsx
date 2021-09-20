53abe20de4629d79e7685927d3c5726141bf0124
jacques-twizeyimana <sandbergjacques500@gmail.com>
2021-09-20 08:44:59 +0200

    feat: intakes page

---
+++ src/components/Organisms/intake/Intakes.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import { CourseModelDataType, Link, ValueType } from '../../../types';
+import Button from '../../Atoms/custom/Button';
+import Cacumber from '../../Molecules/Cacumber';
+import CourseCardMolecule from '../../Molecules/cards/CourseCardMolecule';
+import PopupMolecule from '../../Molecules/Popup';
+import TableHeader from '../../Molecules/table/TableHeader';
+
+const list: Link[] = [
+  { to: 'home', title: 'Institution Admin' },
+  { to: 'faculty', title: 'Faculty' },
+  { to: 'programs', title: 'Programs' },
+  { to: 'intakes', title: 'Intakes' },
+];
+
+const data: CourseModelDataType[] = [
+  {
+    status: { type: 'error', text: 'Inactive' },
+    code: 'Intake-2020',
+    title: 'Cadette Program',
+    subTitle: 'Short course',
+    description:
+      'Start Date:   17 Aug 2021 - 10 Sep 2021 Expected End Date:  17 Aug 2021 - 10 Sep 2021 View More',
+  },
+  {
+    status: { type: 'success', text: 'On going' },
+    code: 'Intake-2020',
+    title: 'Cadette Program',
+    subTitle: 'Short course',
+    description:
+      'Start Date:   17 Aug 2021 - 10 Sep 2021 Expected End Date:  17 Aug 2021 - 10 Sep 2021 View More',
+  },
+  {
+    status: { type: 'success', text: 'On going' },
+    code: 'Intake-2020',
+    title: 'Cadette Program',
+    subTitle: 'Short course',
+    description:
+      'Start Date:   17 Aug 2021 - 10 Sep 2021 Expected End Date:  17 Aug 2021 - 10 Sep 2021 View More',
+  },
+];
+
+export default function Intakes() {
+  function handleSearch(_e: ValueType) {}
+  return (
+    <div>
+      <Cacumber list={list} />
+      <TableHeader title="Intakes" totalItems={7} handleSearch={handleSearch}>
+        <div className="flex gap-3">
+          <Button>Add intake</Button>
+        </div>
+      </TableHeader>{' '}
+      <section className="flex flex-wrap justify-between mt-2">
+        {data.map((course) => (
+          <div key={course.code} className="p-1 mt-3">
+            <CourseCardMolecule
+              data={course}
+              to={{ title: 'module', to: 'modules/id' }}
+            />
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+}
