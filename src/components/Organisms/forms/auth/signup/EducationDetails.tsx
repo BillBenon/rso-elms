6d4f88cf69843d0b8ec6cb91ff9fe9fe23f06b21
loraine <mukezwa@gmail.com>
2021-09-22 16:41:21 -0700

    feat: add panel for each accordion

--- src/components/Organisms/forms/auth/signup/EducationDetails.tsx
+++ src/components/Organisms/forms/auth/signup/EducationDetails.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
 import { ValueType } from '../../../../../types';
-import Accordion from '../../../../Atoms/custom/Accordion';
 import Button from '../../../../Atoms/custom/Button';
 import Icon from '../../../../Atoms/custom/Icon';
+import Panel from '../../../../Atoms/custom/Panel';
 import ILabel from '../../../../Atoms/Text/ILabel';
+import Accordion from '../../../../Molecules/Accordion';
 import DateMolecule from '../../../../Molecules/input/DateMolecule';
 import InputMolecule from '../../../../Molecules/input/InputMolecule';
 interface EducationDataType {
@@ -73,7 +74,7 @@ const EducationDetails = () => {
             value={details.section}
             handleChange={handleChange}>
             Education section/ combination
-            <span className="text-txt-secondary">(Write in full abbreviation)</span>
+            <span className="text-txt-secondary"> (Write in full abbreviation)</span>
           </InputMolecule>
           <DateMolecule handleDate={handleChange} name="start_date" width="60 md:w-80">
             Start Date
@@ -91,11 +92,11 @@ const EducationDetails = () => {
               <span className="text-txt-secondary"> (certificate)</span>
             </ILabel>
           </div>
-          <Button styleType="outline">
-            <div className="flex">
-              <Icon name="attach" stroke="primary" />
+          <Button styleType="outline" className="p-0">
+            <span className="flex items-center">
+              <Icon name="attach" fill="primary" />
               <span className="m-auto font-semibold">Upload</span>
-            </div>
+            </span>
           </Button>
         </div>
         <div>
@@ -110,15 +111,23 @@ const EducationDetails = () => {
       </div>
 
       <div className=" w-80">
-        {educationData.map((educ) => {
-          return (
-            <Accordion key={educ.school} title={educ.school} subtitle={educ.level}>
-              <div className="font-semibold">{educ.section}</div>
-              <div>Start Date: {educ.start_date}</div>
-              <div>End Date: {educ.end_date}</div>
-            </Accordion>
-          );
-        })}
+        <Accordion>
+          {educationData.map((educ) => {
+            return (
+              <Panel key={educ.school} title={educ.school} subtitle={educ.level}>
+                <div className="font-semibold">{educ.section}</div>
+                <div>Start Date: {educ.start_date}</div>
+                <div>End Date: {educ.end_date}</div>
+                <div className="flex items-center">
+                  <Icon name="attach" fill="primary" />
+                  <span className="border-txt-primary border-b font-medium">
+                    Certificate / File name{' '}
+                  </span>
+                </div>
+              </Panel>
+            );
+          })}
+        </Accordion>
       </div>
     </div>
   );
--- src/components/Organisms/programs/ProgramDetails.tsx
+++ src/components/Organisms/programs/ProgramDetails.tsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { CourseModelDataType, Link } from '../../../types';
+import { CommonCardDataType, Link } from '../../../types';
 import Badge from '../../Atoms/custom/Badge';
 import Button from '../../Atoms/custom/Button';
 import Heading from '../../Atoms/Text/Heading';
 import Cacumber from '../../Molecules/Cacumber';
 import AcademyProfileCard from '../../Molecules/cards/AcademyProfileCard';
-import CourseCardMolecule from '../../Molecules/cards/CourseCardMolecule';
+import CourseCardMolecule from '../../Molecules/cards/CommonCardMolecule';
 import PopupMolecule from '../../Molecules/Popup';
 import TableHeader from '../../Molecules/table/TableHeader';
 import { Tab, Tabs } from '../../Molecules/tabs/tabs';
 import NewAcademicProgram from '../forms/programs/NewAcademicProgram';
 
-interface IProgramData extends CourseModelDataType {}
+interface IProgramData extends CommonCardDataType {}
 
 export default function ProgramDetailsMolecule() {
   const history = useHistory();
