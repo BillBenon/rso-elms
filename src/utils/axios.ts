ea39f15074edd875c87bba3487cadc5d59c0d8e8
claranceliberi <liberintwari@gmail.com>
2021-09-23 10:01:11 +0200

    feat: handling axios response error

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,4 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import toast from 'react-hot-toast';
+
+import cookie from './cookie';
 
 const commonConfig: AxiosRequestConfig = {};
 
@@ -7,4 +10,20 @@ const administrationModuleConfig: AxiosRequestConfig = {
   baseURL: 'http://197.243.110.147:8080/administration-service/api',
 };
 
-export const adminstrationAxios = axios.create(administrationModuleConfig);
+const adminstrationAxios = axios.create(administrationModuleConfig);
+
+const interceptAdminReq = (config: AxiosRequestConfig) => {
+  console.log(cookie.getCookie('jwt_token'));
+  return config;
+};
+
+const interceptAdminResError = (error: any) => {
+  const { data } = error.response;
+  toast.error(data.message);
+  // return Promise.reject(error);
+};
+
+adminstrationAxios.interceptors.request.use(interceptAdminReq);
+adminstrationAxios.interceptors.response.use((config) => config, interceptAdminResError);
+
+export { adminstrationAxios };
