dea9781b63de5cb73de0fa6c7d27b66cb6728b1a
blessinghirwa <hirwablessing7@gmail.com>
2021-09-24 11:53:15 +0200

    feat: add hover tooltip on a program

--- src/components/Organisms/programs/AcademicPrograms.tsx
+++ src/components/Organisms/programs/AcademicPrograms.tsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { CommonCardDataType, Link } from '../../../types';
+import Avatar from '../../Atoms/custom/Avatar';
 import Button from '../../Atoms/custom/Button';
+import Heading from '../../Atoms/Text/Heading';
 import Cacumber from '../../Molecules/Cacumber';
 import CommonCardMolecule from '../../Molecules/cards/CommonCardMolecule';
 import PopupMolecule from '../../Molecules/Popup';
 import TableHeader from '../../Molecules/table/TableHeader';
+import Tooltip from '../../Molecules/Tooltip';
 import NewAcademicProgram from '../forms/programs/NewAcademicProgram';
 
 interface IProgramData extends CommonCardDataType {}
@@ -66,12 +69,106 @@ export default function AcademicProgram() {
         </section>
         <section className="flex flex-wrap justify-between mt-2">
           {data.map((Common) => (
-            <div key={Common.code} className="p-1 mt-3">
-              <CommonCardMolecule
-                data={Common}
-                to={{ title: 'module', to: 'programs/3' }}
-              />
-            </div>
+            <Tooltip
+              key={Common.code}
+              trigger={
+                <div className="p-1 mt-3">
+                  <CommonCardMolecule
+                    data={Common}
+                    to={{ title: 'module', to: 'programs/3' }}
+                  />
+                </div>
+              }
+              open>
+              {' '}
+              <div className="w-96">
+                <Heading fontSize="sm" fontWeight="semibold" className="mb-4">
+                  Cadette Program
+                </Heading>
+
+                <div className="flex gap-24">
+                  {/* first column */}
+
+                  <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-2">
+                      <Heading color="txt-secondary" fontSize="sm">
+                        Faculty
+                      </Heading>
+                      <Heading fontSize="sm" fontWeight="semibold">
+                        Military Department
+                      </Heading>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <Heading color="txt-secondary" fontSize="sm">
+                        Modules
+                      </Heading>
+                      <Heading fontSize="sm" fontWeight="semibold">
+                        30
+                      </Heading>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                      <Heading color="txt-secondary" fontSize="sm">
+                        Intakes
+                      </Heading>
+                      <Heading fontSize="sm" fontWeight="semibold">
+                        3
+                      </Heading>
+                    </div>
+                  </div>
+
+                  {/* second column */}
+                  <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-2">
+                      <Heading color="txt-secondary" fontSize="sm">
+                        Levels
+                      </Heading>
+                      <Heading fontSize="sm" fontWeight="semibold">
+                        10
+                      </Heading>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                      <Heading color="txt-secondary" fontSize="sm">
+                        Attendees
+                      </Heading>
+                      <Heading fontSize="sm" fontWeight="semibold">
+                        230
+                      </Heading>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                      <Heading color="txt-secondary" fontSize="sm">
+                        Instructor in charge
+                      </Heading>
+                      <div className="flex items-center gap-2">
+                        <Avatar
+                          alt="program oic"
+                          size="32"
+                          src="https://cdn.britannica.com/w:400,h:300,c:crop/26/157026-050-08ED6418/Sylvester-Stallone-1998.jpg"
+                        />
+                        <Heading fontSize="sm">Captail Liberi</Heading>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* remarks section */}
+                <div className="flex flex-col mt-8 gap-4">
+                  <Heading fontSize="sm" fontWeight="semibold">
+                    Remarks
+                  </Heading>
+                  <Heading fontSize="sm" color="txt-secondary">
+                    This is a training description. It states briefy what this course is
+                    all about. This is a training description. It states briefy what this
+                    course is all about.
+                  </Heading>
+                </div>
+                <div className="mt-4">
+                  <Button onClick={() => history.push('/programs/3')}>View More</Button>
+                </div>
+              </div>
+            </Tooltip>
           ))}
         </section>
 
