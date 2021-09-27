b4d97c5d62bfa2e4a74b3c3c61412db25c603849
claranceliberi <liberintwari@gmail.com>
2021-09-27 11:44:33 +0200

    fix: never add bearer on open requests

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -4,6 +4,8 @@ import toast from 'react-hot-toast';
 import { LoginRes } from '../types';
 import cookie from '../utils/cookie';
 
+const openRequests: string[] = ['/authentication/signin'];
+
 const commonConfig: AxiosRequestConfig = {};
 
 const administrationModuleConfig: AxiosRequestConfig = {
@@ -15,9 +17,14 @@ const adminstrationAxios = axios.create(administrationModuleConfig);
 
 const interceptAdminReq = (config: AxiosRequestConfig) => {
   const token = cookie.getCookie('jwt_info');
-  if (token) {
-    const jwtInfo: LoginRes = JSON.parse(token);
-    config.headers.Authorization = `Bearer ${jwtInfo.token}`;
+  console.log();
+
+  // when request is open no need to add bearer token
+  if (!openRequests.find((link) => link === config.url)) {
+    if (token) {
+      const jwtInfo: LoginRes = JSON.parse(token);
+      config.headers.Authorization = `Bearer ${jwtInfo.token}`;
+    }
   }
 
   return config;
