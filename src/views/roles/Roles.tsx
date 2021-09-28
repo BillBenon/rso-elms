1c003a517568333a9118b5673cf84513cfac8073
claranceliberi <liberintwari@gmail.com>
2021-09-28 12:42:14 +0200

    feat: roles

--- src/views/roles/Roles.tsx
+++ src/views/roles/Roles.tsx
@@ -7,7 +7,6 @@ import PopupMolecule from '../../components/Molecules/Popup';
 import Table from '../../components/Molecules/table/Table';
 import TableHeader from '../../components/Molecules/table/TableHeader';
 import NewRole from '../../components/Organisms/forms/roles/NewRole';
-import Dashboard from '../../layout/Dashboard';
 import { roleStore } from '../../store';
 
 export default function Roles() {
@@ -24,36 +23,34 @@ export default function Roles() {
   function handleSearch() {}
 
   return (
-    <Dashboard>
-      <main>
-        <section>
-          <Cacumber list={[{ title: 'Roles', to: 'roles' }]} />
-        </section>
-        <section>
-          <TableHeader title="Roles" totalItems={4} handleSearch={handleSearch}>
-            <Link to={`${url}/add`}>
-              <Button>Add Role</Button>
-            </Link>
-          </TableHeader>
-        </section>
-        <section>
-          {/* <Table statusColumn="status" data={roles} hasAction={true} /> */}
-        </section>
+    <main>
+      <section>
+        <Cacumber list={[{ title: 'Roles', to: 'roles' }]} />
+      </section>
+      <section>
+        <TableHeader title="Roles" totalItems={4} handleSearch={handleSearch}>
+          <Link to={`${url}/add`}>
+            <Button>Add Role</Button>
+          </Link>
+        </TableHeader>
+      </section>
+      <section>
+        <Table statusColumn="status" data={roles} hasAction={true} />
+      </section>
 
-        <Switch>
-          <Route
-            exact
-            path={`${path}/add`}
-            render={() => {
-              return (
-                <PopupMolecule title="New Role" open={true} onClose={history.goBack}>
-                  <NewRole onSubmit={submited} />
-                </PopupMolecule>
-              );
-            }}
-          />
-        </Switch>
-      </main>
-    </Dashboard>
+      <Switch>
+        <Route
+          exact
+          path={`${path}/add`}
+          render={() => {
+            return (
+              <PopupMolecule title="New Role" open={true} onClose={history.goBack}>
+                <NewRole onSubmit={submited} />
+              </PopupMolecule>
+            );
+          }}
+        />
+      </Switch>
+    </main>
   );
 }
