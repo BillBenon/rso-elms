9cf366fdb8da9c3e968446047cd935bec57a91ca
loraine <mukezwa@gmail.com>
2021-09-29 09:34:48 -0700

    fix: academy add lost work

--- src/views/academies/NewAcademy.tsx
+++ src/views/academies/NewAcademy.tsx
@@ -1,16 +1,65 @@
 import '../../styles/components/Organisms/academy/academy.scss';
 
-import React from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 
 import Button from '../../components/Atoms/custom/Button';
 import Icon from '../../components/Atoms/custom/Icon';
 import Heading from '../../components/Atoms/Text/Heading';
 import ILabel from '../../components/Atoms/Text/ILabel';
 import InputMolecule from '../../components/Molecules/input/InputMolecule';
-import TextAreaMolecule from '../../components/Molecules/input/TextAreaMolecule';
+import { authenticatorService } from '../../services';
+import academyStore from '../../store/academy.store';
+import { ValueType } from '../../types';
+import { AcademyCreateInfo } from '../../types/services/academy.types';
 
 export default function AddAcademy() {
-  function handleChange(_event: any) {}
+  const history = useHistory();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await authenticatorService.authUser();
+      setDetails((details) => ({
+        ...details,
+        current_admin_id: user.data.data.id,
+      }));
+    };
+
+    getUser();
+  }, []);
+
+  const [details, setDetails] = useState<AcademyCreateInfo>({
+    current_admin_id: '',
+    email: '',
+    fax_number: '',
+    full_address: '',
+    head_office_location_id: 17445,
+    institution_id: 'b832407f-fb77-4a75-8679-73bf7794f207',
+    mission: '',
+    moto: '',
+    name: '',
+    phone_number: '',
+    postal_code: '',
+    short_name: '',
+    website_link: '',
+  });
+  const { mutateAsync } = academyStore.createAcademy();
+
+  function handleChange(e: ValueType) {
+    setDetails((details) => ({
+      ...details,
+      [e.name]: e.value,
+    }));
+  }
+
+  async function createAcademy<T>(e: FormEvent<T>) {
+    e.preventDefault();
+    await mutateAsync(details, {
+      onSuccess() {
+        history.push('/academies');
+      },
+    });
+  }
 
   return (
     <>
@@ -35,52 +84,87 @@ export default function AddAcademy() {
             New academy
           </Heading>
         </div>
-        <InputMolecule
-          name="acname"
-          placeholder="Type academy name"
-          value={''}
-          handleChange={(e) => handleChange(e)}>
-          academy name
-        </InputMolecule>
-        <InputMolecule
-          name="email"
-          type="email"
-          value={''}
-          placeholder="example@gmail.com"
-          handleChange={(e) => handleChange(e)}>
-          academy email
-        </InputMolecule>
-        <InputMolecule
-          name="phone"
-          type="tel"
-          value={''}
-          placeholder="Type academy phone number"
-          handleChange={(e) => handleChange(e)}>
-          academy phone number
-        </InputMolecule>
-        <InputMolecule
-          name="website"
-          value={''}
-          placeholder="Type website url"
-          handleChange={(e) => handleChange(e)}>
-          academy website
-        </InputMolecule>
-        <TextAreaMolecule
-          name="website"
-          value={''}
-          placeholder="Type website url"
-          handleChange={(e) => handleChange(e)}>
-          academy description
-        </TextAreaMolecule>
-        <div>
-          <div className="mb-3">
-            <ILabel weight="bold">academy logo</ILabel>
+        <form onSubmit={createAcademy}>
+          <InputMolecule
+            name="name"
+            placeholder="Type academy name"
+            value={details.name}
+            handleChange={(e) => handleChange(e)}>
+            academy name
+          </InputMolecule>
+          <InputMolecule
+            name="short_name"
+            placeholder="Type short name"
+            value={details.short_name}
+            handleChange={(e) => handleChange(e)}>
+            academy short name
+          </InputMolecule>
+          <InputMolecule
+            name="email"
+            type="email"
+            value={details.email}
+            placeholder="example@gmail.com"
+            handleChange={(e) => handleChange(e)}>
+            academy email
+          </InputMolecule>
+          <InputMolecule
+            name="phone_number"
+            type="tel"
+            value={details.phone_number}
+            placeholder="Type academy phone number"
+            handleChange={(e) => handleChange(e)}>
+            academy phone number
+          </InputMolecule>
+          <InputMolecule
+            name="fax_number"
+            value={details.fax_number}
+            handleChange={(e) => handleChange(e)}>
+            academy fax number
+          </InputMolecule>
+          <InputMolecule
+            name="full_address"
+            value={details.full_address}
+            handleChange={(e) => handleChange(e)}>
+            academy physical address
+          </InputMolecule>
+          <InputMolecule
+            name="website_link"
+            value={details.website_link}
+            placeholder="Type website url"
+            handleChange={(e) => handleChange(e)}>
+            academy website
+          </InputMolecule>
+          <InputMolecule
+            name="mission"
+            value={details.mission}
+            handleChange={(e) => handleChange(e)}>
+            academy mission
+          </InputMolecule>
+          <InputMolecule
+            name="moto"
+            value={details.moto}
+            handleChange={(e) => handleChange(e)}>
+            academy motto
+          </InputMolecule>
+          {/* <TextAreaMolecule
+            name="website"
+            value={''}
+            placeholder="Type website url"
+            handleChange={(e) => handleChange(e)}>
+            academy description
+          </TextAreaMolecule> */}
+          <div>
+            <div className="mb-3">
+              <ILabel weight="bold">academy logo</ILabel>
+            </div>
+            <Button styleType="outline">upload logo</Button>
           </div>
-          <Button styleType="outline">upload logo</Button>
-        </div>
-        <div className="mt-5">
-          <Button full>Save</Button>
-        </div>
+          <div className="mt-5">
+            <Button full type="submit">
+              Save
+            </Button>
+          </div>
+        </form>
       </div>
     </>
   );
