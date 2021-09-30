6f185daecd1ae40678f479873617a25186a85acc
claranceliberi <liberintwari@gmail.com>
2021-09-30 22:00:43 +0200

    feat: viewing roles with actionable roles

--- src/views/roles/ViewRole.tsx
+++ src/views/roles/ViewRole.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import Button from '../../components/Atoms/custom/Button';
+import Icon from '../../components/Atoms/custom/Icon';
 import Heading from '../../components/Atoms/Text/Heading';
+import ActionableList from '../../components/Molecules/ActionableList';
 import Cacumber from '../../components/Molecules/Cacumber';
 import { roleStore } from '../../store';
 
@@ -29,20 +32,52 @@ export default function ViewRole() {
         <div>{isError && error?.message} </div>
 
         {isSuccess && role && (
-          <div>
-            <div className="pt-3">
-              <Heading fontSize="lg" color="txt-secondary">
-                Role name
-              </Heading>
-              <p className="pt-2">{role.name}</p>
+          <>
+            <div>
+              <div className="pt-3">
+                <Heading fontSize="lg" color="txt-secondary">
+                  Role name
+                </Heading>
+                <p className="pt-2">{role.name}</p>
+              </div>
+              <div className="pt-3">
+                <Heading fontSize="lg" color="txt-secondary">
+                  Role description
+                </Heading>
+                <p className="pt-2"> {role.name} </p>
+              </div>
             </div>
-            <div className="pt-3">
-              <Heading fontSize="lg" color="txt-secondary">
-                Role description
-              </Heading>
-              <p className="pt-2"> {role.name} </p>
+            <div className="width28 mt-10 bg-main py-2 rounded-lg">
+              <div className="flex items-center justify-between pl-6">
+                <Heading fontWeight="semibold">Privileges</Heading>
+                <Button styleType="text">
+                  <span className="flex items-center">
+                    <Icon size={13} name="add" />
+                    <span className="pl-1">add privilege</span>
+                  </span>
+                </Button>
+              </div>
+              <div>
+                <ul>
+                  <li>
+                    <ActionableList>CAN_CREATE_USER</ActionableList>
+                  </li>
+                  <li>
+                    <ActionableList>CAN_CREATE_USER</ActionableList>
+                  </li>
+                  <li>
+                    <ActionableList>CAN_CREATE_USER</ActionableList>
+                  </li>
+                  <li>
+                    <ActionableList>CAN_CREATE_USER</ActionableList>
+                  </li>
+                  <li>
+                    <ActionableList>CAN_CREATE_USER</ActionableList>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
+          </>
         )}
       </section>
     </main>
