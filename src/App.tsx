e96a8c6583b8d103ed4bf17a2eb53a28cbfd6473
jacques-twizeyimana <sandbergjacques500@gmail.com>
2021-09-30 12:55:34 +0200

    fix: remove unsed imports

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ import IntakesView from './views/intakes/Intakes';
 import LevelsView from './views/levels/Levels';
 import Modules from './views/modules';
 import NotFound from './views/NotFound';
-import Popup from './views/Popup';
 import AcademicPrograms from './views/programs/AcademicPrograms';
 import NewAcademicProgram from './views/programs/NewAcademicProgram';
 import ProgramDetails from './views/programs/ProgramDetails';
@@ -43,7 +42,6 @@ const App = () => {
             <Dashboard>
               <Route path="/dashboard/academies" component={Academies} />
               <Route path="/dashboard/roles" component={Roles} />
-              <Route exact path="/dashboard/popup" component={Popup} />
               <Route exact path="/dashboard/modules" component={Modules} />
               <Route exact path="/dashboard/subjects" component={Subjects} />
               <Route
@@ -51,13 +49,14 @@ const App = () => {
                 path="/dashboard/registration-control"
                 component={RegistrationControl}
               />
-              <Route path="/dashboard/users" component={Users} />
-              <Route exact path="/dashboard/user/new" component={NewStudent} />
+
+              <Route exact path="/dashboard/users/student/new" component={NewStudent} />
               <Route
                 exact
-                path="/dashboard/user/instructor/new"
+                path="/dashboard/users/instructor/new"
                 component={NewInstructor}
               />
+              <Route path="/dashboard/users" component={Users} />
               <Route exact path="/dashboard/faculties" component={FacultiesView} />
               <Route exact path="/dashboard/programs" component={AcademicPrograms} />
               <Route
@@ -65,7 +64,7 @@ const App = () => {
                 path="/dashboard/programs/new"
                 component={NewAcademicProgram}
               />
-              <Route path="/dashboard/programs/:id" component={ProgramDetails} />
+              <Route exact path="/dashboard/programs/:id" component={ProgramDetails} />
               <Route exact path="/dashboard/levels" component={LevelsView} />
               <Route exact path="/dashboard/intakes" component={IntakesView} />
               <Route exact path="/dashboard/intakes/:id" component={IntakeModulesView} />
--- src/types/services/intake.types.ts
+++ src/types/services/intake.types.ts
@@ -1,3 +1,5 @@
+import { GenericStatus, Table } from '..';
+
 export interface IntakeInfo {
   actual_end_date: Date;
   actual_start_date: Date;
@@ -5,15 +7,18 @@ export interface IntakeInfo {
   description: string;
   expected_end_date: Date;
   expected_start_date: Date;
-  id: string;
+  id: string | number;
   intake_status: IntakeStatus;
   period_type: PeriodType;
   registration_control_id: string;
   total_num_students: number;
 }
 
-export interface TableId {
-  id: string;
+export interface ExtendedIntakeInfo extends Table, IntakeInfo {
+  registration_control: Object;
+  generic_status: GenericStatus;
+  last_status_change_reason: string;
+  registration_control_id: string;
 }
 
 export enum IntakeStatus {
