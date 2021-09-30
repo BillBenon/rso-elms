0e651b0d76a39646f5d793f7102dbaba6ae2c250
loraine <mukezwa@gmail.com>
2021-09-30 14:38:56 -0700

    refactor: remove program selection popup

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,6 @@ import Modules from './views/modules';
 import NotFound from './views/NotFound';
 import Popup from './views/Popup';
 import AcademicPrograms from './views/programs/AcademicPrograms';
-import NewAcademicProgram from './views/programs/NewAcademicProgram';
-import ProgramDetails from './views/programs/ProgramDetails';
 import Roles from './views/roles/Roles';
 import Subjects from './views/subjects';
 import Users from './views/users/Users';
@@ -57,13 +55,7 @@ const App = () => {
                 component={NewInstructor}
               />
               <Route exact path="/dashboard/faculties" component={FacultiesView} />
-              <Route exact path="/dashboard/programs" component={AcademicPrograms} />
-              <Route
-                exact
-                path="/dashboard/programs/new"
-                component={NewAcademicProgram}
-              />
-              <Route path="/dashboard/programs/:id" component={ProgramDetails} />
+              <Route path="/dashboard/programs" component={AcademicPrograms} />
               <Route exact path="/dashboard/levels" component={LevelsView} />
               <Route exact path="/dashboard/intakes" component={IntakesView} />
               <Route exact path="/dashboard/intakes/:id" component={IntakeModulesView} />
