e93f166821d03bccf297529fea2c8536c9b336d3
claranceliberi <liberintwari@gmail.com>
2021-10-05 19:42:39 +0200

    feat: disabled

--- src/components/Organisms/registrationControl/RegistrationControl.tsx
+++ src/components/Organisms/registrationControl/RegistrationControl.tsx
@@ -96,7 +96,7 @@ export default function RegistrationControl() {
         totalItems={RegistrationControls.length}
         handleSearch={handleSearch}>
         <Link to={`${url}/add`}>
-          <Button>Add new reg control</Button>
+          <Button disabled>Add new reg control</Button>
         </Link>
       </TableHeader>
 
