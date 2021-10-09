5f43d71d9a8d8f22f33375a476c5b264ce0f5f40
claranceliberi <liberintwari@gmail.com>
2021-10-09 07:48:05 +0200

    fix: pro

--- src/views/programs/AcademicPrograms.tsx
+++ src/views/programs/AcademicPrograms.tsx
@@ -1,5 +1,5 @@
 import queryString from 'query-string';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {
   Link,
   Route,
@@ -19,10 +19,13 @@ import NoDataAvailable from '../../components/Molecules/cards/NoDataAvailable';
 import PopupMolecule from '../../components/Molecules/Popup';
 import TableHeader from '../../components/Molecules/table/TableHeader';
 import Tooltip from '../../components/Molecules/Tooltip';
+import { intakeStore } from '../../store/intake.store';
 import programStore from '../../store/program.store';
 import { CommonCardDataType, Link as LinkList } from '../../types';
 import { DivisionInfo } from '../../types/services/division.types';
+import { IntakeProgramInfo, ProgramInfo } from '../../types/services/program.types';
 import { advancedTypeChecker } from '../../utils/getOption';
+import AddAcademicProgramToIntake from './AddAcademicProgramToIntake';
 import NewAcademicProgram from './NewAcademicProgram';
 import ProgramDetails from './ProgramDetails';
 import UpdateAcademicProgram from './UpdateAcademicProgram';
@@ -35,14 +38,9 @@ export interface IProgramData extends CommonCardDataType {
 export default function AcademicProgram() {
   const { url, path } = useRouteMatch();
   const history = useHistory();
+  const { search } = useLocation();
+  const intakeId = new URLSearchParams(search).get('intakeId');
   const location = useLocation();
-
-  const [prOpen, setPrOpen] = useState(false); // state to controll the popup
-
-  //eslint-disable-next-line
-  function submited() {
-    setPrOpen(true);
-  }
 
   const list: LinkList[] = [
     { to: 'home', title: 'home' },
@@ -51,12 +49,22 @@ export default function AcademicProgram() {
     { to: `${url}`, title: 'Programs' },
   ];
 
-  const { data, refetch } = programStore.fetchPrograms();
   const queryStr = queryString.parse(location.search);
-  const { data: programData } = programStore.getProgramsByDepartment(
-    queryStr.query?.toString() || '',
-  );
-  const programInfo = programData || data;
+
+  const { data, refetch } = intakeId
+    ? intakeStore.getProgramsByIntake(intakeId)
+    : queryStr
+    ? programStore.getProgramsByDepartment(queryStr.query?.toString() || '')
+    : programStore.fetchPrograms();
+
+  const programInfo = data?.data.data;
+
+  const intake = intakeStore.getIntakeById(intakeId!, true);
+
+  // fetch intake if id is available
+  if (intakeId && !intake.isSuccess && !intake.isLoading) intake.refetch();
+
+  // const programInfo = programData || data;
 
   useEffect(() => {
     if (location.pathname === path || location.pathname === `${path}/`) {
@@ -66,7 +74,11 @@ export default function AcademicProgram() {
   }, [location]);
 
   let programs: IProgramData[] = [];
-  programInfo?.data.data.map((obj) => {
+
+  programInfo?.map((obj) => {
+    if (intakeId) obj = (obj as IntakeProgramInfo).program;
+    else obj = obj as ProgramInfo;
+
     let { id, code, name, description, generic_status, department, incharge, type } = obj;
 
     let prog: IProgramData = {
@@ -86,29 +98,151 @@ export default function AcademicProgram() {
     programs.push(prog);
   });
 
+  function submited() {
+    refetch();
+    history.goBack();
+  }
+
   return (
-    <Switch>
-      <Route path={`${path}/:id/details`} render={() => <ProgramDetails />} />
-      <main className="px-4">
-        <section>
-          <Cacumber list={list}></Cacumber>
-        </section>
-        <section>
-          <TableHeader
-            totalItems={intakeId ? `${programs.length} programs` : programs.length}
-            title={`${intakeId ? intake.data?.data.data.title : 'Programs'}`}
-            showSearch={false}>
-            {intakeId ? (
-              <Link to={`${url}/add-program-to-intake?intakeId=${intakeId}`}>
-                <Button>Add Program To Intake</Button>
-              </Link>
-            ) : (
-              <Link to={`${url}/add`}>
-                <Button>Add New Program</Button>
-              </Link>
-            )}
-          </TableHeader>
-        </section>
+    <main className="px-4">
+      <section>
+        <Cacumber list={list}></Cacumber>
+      </section>
+      <section>
+        <TableHeader
+          totalItems={intakeId ? `${programs.length} programs` : programs.length}
+          title={`${intakeId ? intake.data?.data.data.title : 'Programs'}`}
+          showSearch={false}>
+          {intakeId ? (
+            <Link to={`${url}/add-program-to-intake?intakeId=${intakeId}`}>
+              <Button>Add Program To Intake</Button>
+            </Link>
+          ) : (
+            <Link to={`${url}/add`}>
+              <Button>Add New Program</Button>
+            </Link>
+          )}
+        </TableHeader>
+      </section>
+      <Route
+        exact
+        path={`${path}`}
+        render={() => {
+          return (
+            <section className="flex flex-wrap justify-between mt-2">
+              {programs.length ? (
+                programs.map((Common) => (
+                  <Tooltip
+                    key={Common.code}
+                    trigger={
+                      <div className="p-1 mt-3">
+                        <CommonCardMolecule
+                          data={Common}
+                          to={{ title: 'module', to: `programs/${Common.id}` }}
+                        />
+                      </div>
+                    }
+                    open>
+                    <div className="w-96">
+                      <CardHeadMolecule
+                        title={Common.title}
+                        code={Common.code}
+                        status={Common.status}
+                        description={''}
+                      />
+
+                      {/* first column */}
+
+                      <div className="flex flex-col gap-6">
+                        <div className="flex flex-col gap-2">
+                          <Heading color="txt-secondary" fontSize="sm">
+                            {Common.department.division_type}
+                          </Heading>
+                          <Heading fontSize="sm" fontWeight="semibold">
+                            {Common.department.name}
+                          </Heading>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                          <Heading color="txt-secondary" fontSize="sm">
+                            Modules
+                          </Heading>
+                          <Heading fontSize="sm" fontWeight="semibold">
+                            30
+                          </Heading>
+                        </div>
+
+                        <div className="flex flex-col gap-2">
+                          <Heading color="txt-secondary" fontSize="sm">
+                            Program Type
+                          </Heading>
+                          <Heading fontSize="sm" fontWeight="semibold">
+                            {Common.subTitle}
+                          </Heading>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                          <Heading color="txt-secondary" fontSize="sm">
+                            Instructor in charge
+                          </Heading>
+                          <div className="flex items-center">
+                            <div className="">
+                              <Avatar
+                                size="24"
+                                alt="user1 profile"
+                                className=" rounded-full  border-2 border-main transform hover:scale-125"
+                                src="https://randomuser.me/api/portraits/men/1.jpg"
+                              />
+                            </div>
+                            <Heading fontSize="sm" fontWeight="semibold">
+                              {Common.incharge}
+                            </Heading>
+                          </div>
+                        </div>
+                      </div>
+
+                      {/* remarks section */}
+                      <div className="flex flex-col mt-8 gap-4">
+                        <Heading fontSize="sm" fontWeight="semibold">
+                          Remarks
+                        </Heading>
+                        <Heading fontSize="sm" color="txt-secondary">
+                          {Common.description}
+                        </Heading>
+                      </div>
+                      <div className="mt-4 space-x-4">
+                        <Link to={`${url}/${Common.id}/edit`}>
+                          <Button>Edit program</Button>
+                        </Link>
+                        <Button styleType="outline">Change Status</Button>
+                      </div>
+                    </div>
+                  </Tooltip>
+                ))
+              ) : (
+                <NoDataAvailable
+                  buttonLabel="Add new program"
+                  title={'No program available'}
+                  handleClick={() => history.push(`/dashboard/programs/add`)}
+                  description="And the web just isnt the same without you. Lets get you back online!"
+                />
+              )}
+            </section>
+          );
+        }}
+      />
+      <Switch>
+        {/* add academic program to intake*/}
+        <Route
+          exact
+          path={`${url}/add-program-to-intake`}
+          render={() => {
+            return (
+              <PopupMolecule title="Programs" open={true} onClose={history.goBack}>
+                <AddAcademicProgramToIntake submited={submited} />
+              </PopupMolecule>
+            );
+          }}
+        />
+
         {/* create academic program */}
         <Route
           exact
@@ -128,7 +262,7 @@ export default function AcademicProgram() {
             return (
               <PopupMolecule
                 title="Add prerequisite"
-                open={prOpen}
+                open={true}
                 onClose={history.goBack}>
                 another form here
               </PopupMolecule>
@@ -143,112 +277,9 @@ export default function AcademicProgram() {
           path={`${path}/:id/view-program`}
           render={() => <ViewProgramsInDepartment />}
         /> */}
-        <Route
-          exact
-          path={`${path}`}
-          render={() => {
-            return (
-              <section className="flex flex-wrap justify-between mt-2">
-                {programs.length ? (
-                  programs.map((Common) => (
-                    <Tooltip
-                      key={Common.code}
-                      trigger={
-                        <div className="p-1 mt-3">
-                          <CommonCardMolecule
-                            data={Common}
-                            to={{ title: 'module', to: `programs/${Common.id}` }}
-                          />
-                        </div>
-                      }
-                      open>
-                      <div className="w-96">
-                        <CardHeadMolecule
-                          title={Common.title}
-                          code={Common.code}
-                          status={Common.status}
-                          description={''}
-                        />
 
-                        {/* first column */}
-
-                        <div className="flex flex-col gap-6">
-                          <div className="flex flex-col gap-2">
-                            <Heading color="txt-secondary" fontSize="sm">
-                              {Common.department.division_type}
-                            </Heading>
-                            <Heading fontSize="sm" fontWeight="semibold">
-                              {Common.department.name}
-                            </Heading>
-                          </div>
-                          <div className="flex flex-col gap-2">
-                            <Heading color="txt-secondary" fontSize="sm">
-                              Modules
-                            </Heading>
-                            <Heading fontSize="sm" fontWeight="semibold">
-                              30
-                            </Heading>
-                          </div>
-
-                          <div className="flex flex-col gap-2">
-                            <Heading color="txt-secondary" fontSize="sm">
-                              Program Type
-                            </Heading>
-                            <Heading fontSize="sm" fontWeight="semibold">
-                              {Common.subTitle}
-                            </Heading>
-                          </div>
-                          <div className="flex flex-col gap-2">
-                            <Heading color="txt-secondary" fontSize="sm">
-                              Instructor in charge
-                            </Heading>
-                            <div className="flex items-center">
-                              <div className="">
-                                <Avatar
-                                  size="24"
-                                  alt="user1 profile"
-                                  className=" rounded-full  border-2 border-main transform hover:scale-125"
-                                  src="https://randomuser.me/api/portraits/men/1.jpg"
-                                />
-                              </div>
-                              <Heading fontSize="sm" fontWeight="semibold">
-                                {Common.incharge}
-                              </Heading>
-                            </div>
-                          </div>
-                        </div>
-
-                        {/* remarks section */}
-                        <div className="flex flex-col mt-8 gap-4">
-                          <Heading fontSize="sm" fontWeight="semibold">
-                            Remarks
-                          </Heading>
-                          <Heading fontSize="sm" color="txt-secondary">
-                            {Common.description}
-                          </Heading>
-                        </div>
-                        <div className="mt-4 space-x-4">
-                          <Link to={`${url}/${Common.id}/edit`}>
-                            <Button>Edit program</Button>
-                          </Link>
-                          <Button styleType="outline">Change Status</Button>
-                        </div>
-                      </div>
-                    </Tooltip>
-                  ))
-                ) : (
-                  <NoDataAvailable
-                    buttonLabel="Add new program"
-                    title={'No program available'}
-                    handleClick={() => history.push(`/dashboard/programs/add`)}
-                    description="And the web just isnt the same without you. Lets get you back online!"
-                  />
-                )}
-              </section>
-            );
-          }}
-        />
-      </main>
-    </Switch>
+        <Route exact path={`${path}/:id/details`} render={() => <ProgramDetails />} />
+      </Switch>
+    </main>
   );
 }
