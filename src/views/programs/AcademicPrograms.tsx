b4568c08b1ca6d359aa813ffb304655782ee85a0
claranceliberi <liberintwari@gmail.com>
2021-10-09 07:49:25 +0200

    fix: programs issue

--- src/views/programs/AcademicPrograms.tsx
+++ src/views/programs/AcademicPrograms.tsx
@@ -105,127 +105,129 @@ export default function AcademicProgram() {
 
   return (
     <main className="px-4">
-      <section>
-        <Cacumber list={list}></Cacumber>
-      </section>
-      <section>
-        <TableHeader
-          totalItems={intakeId ? `${programs.length} programs` : programs.length}
-          title={`${intakeId ? intake.data?.data.data.title : 'Programs'}`}
-          showSearch={false}>
-          {intakeId ? (
-            <Link to={`${url}/add-program-to-intake?intakeId=${intakeId}`}>
-              <Button>Add Program To Intake</Button>
-            </Link>
-          ) : (
-            <Link to={`${url}/add`}>
-              <Button>Add New Program</Button>
-            </Link>
-          )}
-        </TableHeader>
-      </section>
       <Route
         exact
         path={`${path}`}
         render={() => {
           return (
-            <section className="flex flex-wrap justify-between mt-2">
-              {programs.length ? (
-                programs.map((Common) => (
-                  <Tooltip
-                    key={Common.code}
-                    trigger={
-                      <div className="p-1 mt-3">
-                        <CommonCardMolecule
-                          data={Common}
-                          to={{ title: 'module', to: `programs/${Common.id}` }}
+            <>
+              <section>
+                <Cacumber list={list}></Cacumber>
+              </section>
+              <section>
+                <TableHeader
+                  totalItems={intakeId ? `${programs.length} programs` : programs.length}
+                  title={`${intakeId ? intake.data?.data.data.title : 'Programs'}`}
+                  showSearch={false}>
+                  {intakeId ? (
+                    <Link to={`${url}/add-program-to-intake?intakeId=${intakeId}`}>
+                      <Button>Add Program To Intake</Button>
+                    </Link>
+                  ) : (
+                    <Link to={`${url}/add`}>
+                      <Button>Add New Program</Button>
+                    </Link>
+                  )}
+                </TableHeader>
+              </section>
+              <section className="flex flex-wrap justify-between mt-2">
+                {programs.length ? (
+                  programs.map((Common) => (
+                    <Tooltip
+                      key={Common.code}
+                      trigger={
+                        <div className="p-1 mt-3">
+                          <CommonCardMolecule
+                            data={Common}
+                            to={{ title: 'module', to: `programs/${Common.id}` }}
+                          />
+                        </div>
+                      }
+                      open>
+                      <div className="w-96">
+                        <CardHeadMolecule
+                          title={Common.title}
+                          code={Common.code}
+                          status={Common.status}
+                          description={''}
                         />
-                      </div>
-                    }
-                    open>
-                    <div className="w-96">
-                      <CardHeadMolecule
-                        title={Common.title}
-                        code={Common.code}
-                        status={Common.status}
-                        description={''}
-                      />
 
-                      {/* first column */}
+                        {/* first column */}
 
-                      <div className="flex flex-col gap-6">
-                        <div className="flex flex-col gap-2">
-                          <Heading color="txt-secondary" fontSize="sm">
-                            {Common.department.division_type}
-                          </Heading>
-                          <Heading fontSize="sm" fontWeight="semibold">
-                            {Common.department.name}
-                          </Heading>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          <Heading color="txt-secondary" fontSize="sm">
-                            Modules
-                          </Heading>
-                          <Heading fontSize="sm" fontWeight="semibold">
-                            30
-                          </Heading>
-                        </div>
-
-                        <div className="flex flex-col gap-2">
-                          <Heading color="txt-secondary" fontSize="sm">
-                            Program Type
-                          </Heading>
-                          <Heading fontSize="sm" fontWeight="semibold">
-                            {Common.subTitle}
-                          </Heading>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          <Heading color="txt-secondary" fontSize="sm">
-                            Instructor in charge
-                          </Heading>
-                          <div className="flex items-center">
-                            <div className="">
-                              <Avatar
-                                size="24"
-                                alt="user1 profile"
-                                className=" rounded-full  border-2 border-main transform hover:scale-125"
-                                src="https://randomuser.me/api/portraits/men/1.jpg"
-                              />
-                            </div>
+                        <div className="flex flex-col gap-6">
+                          <div className="flex flex-col gap-2">
+                            <Heading color="txt-secondary" fontSize="sm">
+                              {Common.department.division_type}
+                            </Heading>
                             <Heading fontSize="sm" fontWeight="semibold">
-                              {Common.incharge}
+                              {Common.department.name}
                             </Heading>
                           </div>
+                          <div className="flex flex-col gap-2">
+                            <Heading color="txt-secondary" fontSize="sm">
+                              Modules
+                            </Heading>
+                            <Heading fontSize="sm" fontWeight="semibold">
+                              30
+                            </Heading>
+                          </div>
+
+                          <div className="flex flex-col gap-2">
+                            <Heading color="txt-secondary" fontSize="sm">
+                              Program Type
+                            </Heading>
+                            <Heading fontSize="sm" fontWeight="semibold">
+                              {Common.subTitle}
+                            </Heading>
+                          </div>
+                          <div className="flex flex-col gap-2">
+                            <Heading color="txt-secondary" fontSize="sm">
+                              Instructor in charge
+                            </Heading>
+                            <div className="flex items-center">
+                              <div className="">
+                                <Avatar
+                                  size="24"
+                                  alt="user1 profile"
+                                  className=" rounded-full  border-2 border-main transform hover:scale-125"
+                                  src="https://randomuser.me/api/portraits/men/1.jpg"
+                                />
+                              </div>
+                              <Heading fontSize="sm" fontWeight="semibold">
+                                {Common.incharge}
+                              </Heading>
+                            </div>
+                          </div>
+                        </div>
+
+                        {/* remarks section */}
+                        <div className="flex flex-col mt-8 gap-4">
+                          <Heading fontSize="sm" fontWeight="semibold">
+                            Remarks
+                          </Heading>
+                          <Heading fontSize="sm" color="txt-secondary">
+                            {Common.description}
+                          </Heading>
+                        </div>
+                        <div className="mt-4 space-x-4">
+                          <Link to={`${url}/${Common.id}/edit`}>
+                            <Button>Edit program</Button>
+                          </Link>
+                          <Button styleType="outline">Change Status</Button>
                         </div>
                       </div>
-
-                      {/* remarks section */}
-                      <div className="flex flex-col mt-8 gap-4">
-                        <Heading fontSize="sm" fontWeight="semibold">
-                          Remarks
-                        </Heading>
-                        <Heading fontSize="sm" color="txt-secondary">
-                          {Common.description}
-                        </Heading>
-                      </div>
-                      <div className="mt-4 space-x-4">
-                        <Link to={`${url}/${Common.id}/edit`}>
-                          <Button>Edit program</Button>
-                        </Link>
-                        <Button styleType="outline">Change Status</Button>
-                      </div>
-                    </div>
-                  </Tooltip>
-                ))
-              ) : (
-                <NoDataAvailable
-                  buttonLabel="Add new program"
-                  title={'No program available'}
-                  handleClick={() => history.push(`/dashboard/programs/add`)}
-                  description="And the web just isnt the same without you. Lets get you back online!"
-                />
-              )}
-            </section>
+                    </Tooltip>
+                  ))
+                ) : (
+                  <NoDataAvailable
+                    buttonLabel="Add new program"
+                    title={'No program available'}
+                    handleClick={() => history.push(`/dashboard/programs/add`)}
+                    description="And the web just isnt the same without you. Lets get you back online!"
+                  />
+                )}
+              </section>
+            </>
           );
         }}
       />
