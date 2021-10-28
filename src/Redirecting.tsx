bdc9eb2c8c58759fa40d86ee8a7feb1eba3189c5
loraine <mukezwa@gmail.com>
2021-10-28 10:12:06 -0700

    refactor(not approved): show the right academy

--- src/Redirecting.tsx
+++ src/Redirecting.tsx
@@ -90,11 +90,13 @@ export default function Redirecting() {
         )}
 
         {/* when academic admin does not have academy assigned to him */}
-        {hasNoAcademy &&
-          ErrorCard({
-            text: 'User %% has no Academy to operate in, please contact admin',
-            value: data?.data.data.user_type,
-          })}
+        {hasNoAcademy && (
+          // ErrorCard({
+          //   text: 'User %% has no Academy to operate in, please contact admin',
+          //   value: data?.data.data.user_type,
+          // })
+          <NotApproved />
+        )}
 
         {/* when user type is not yet supported in system */}
         {userNotAllowed && (
@@ -118,35 +120,35 @@ export default function Redirecting() {
   );
 }
 
-interface ErrorCardProp {
-  text: string;
-  value: string | undefined;
-}
+// interface ErrorCardProp {
+//   text: string;
+//   value: string | undefined;
+// }
 
-function ErrorCard({ text, value }: ErrorCardProp) {
-  const spanValue = <span className="bg-error-400 px-2 rounded">{value}</span>;
+// function ErrorCard({ text, value }: ErrorCardProp) {
+//   const spanValue = <span className="bg-error-400 px-2 rounded">{value}</span>;
 
-  const realError = text.split('%%');
+//   const realError = text.split('%%');
 
-  return (
-    <div className="full-height w-full grid items-center">
-      <div className="flex justify-center">
-        <div className=" ">
-          <div className="flex items-center px-6 py-1 rounded-lg bg-tertiary">
-            <Icon name="alert" stroke="error" />
-            <p>
-              {' '}
-              {realError[0]} {spanValue} {realError[1]}{' '}
-            </p>
-          </div>
+//   return (
+//     <div className="full-height w-full grid items-center">
+//       <div className="flex justify-center">
+//         <div className=" ">
+//           <div className="flex items-center px-6 py-1 rounded-lg bg-tertiary">
+//             <Icon name="alert" stroke="error" />
+//             <p>
+//               {' '}
+//               {realError[0]} {spanValue} {realError[1]}{' '}
+//             </p>
+//           </div>
 
-          <div className="flex justify-center pt-5 pb-3">
-            <Link to="/login">
-              <Button>Go Back Home</Button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+//           <div className="flex justify-center pt-5 pb-3">
+//             <Link to="/login">
+//               <Button>Go Back Home</Button>
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
