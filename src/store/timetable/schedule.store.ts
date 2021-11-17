487e3d3f75e016f0d735e2a72fd253d352c8020e
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2021-11-17 22:09:34 +0200

    feat: create schedule store

--- src/store/timetable/schedule.store.ts
+++ src/store/timetable/schedule.store.ts
@@ -1,17 +1,45 @@
 import { useMutation, useQuery } from 'react-query';
 
 import { scheduleService } from '../../services/timetable/schedule.service';
+import { ScheduleStatus } from '../../types/services/schedule.types';
 
 class ScheduleStore {
-  createSchedule() {
-    return useMutation(scheduleService.createSchedule);
+  createDateRangeEvents() {
+    return useMutation(scheduleService.createDateRangeEvents);
+  }
+  createOneTimeEvents() {
+    return useMutation(scheduleService.createOneTimeEvents);
+  }
+  createRecurringEvents() {
+    return useMutation(scheduleService.createRecurringEvents);
   }
 
   getScheduleById(id: string) {
-    return useQuery(['schedules/id', id], () => scheduleService.getSchedule(id));
+    return useQuery(['schedules/id', id], () => scheduleService.getScheduleById(id));
   }
+
   getAllSchedules() {
-    return useQuery(['schedules'], scheduleService.getAllSchedules);
+    return useQuery(['schedules'], scheduleService.getAll);
+  }
+  getAllByAcademicProgramIntakeLevel(id: string) {
+    return useQuery(['schedules/level-intake/:id', id], () =>
+      scheduleService.getAllByAcademicProgramIntakeLevel(id),
+    );
+  }
+  getAllByAcademicProgramIntakeLevelAndStatus(id: string, status: ScheduleStatus) {
+    return useQuery(['schedules/level-intake/:id/:status', id, status], () =>
+      scheduleService.getAllByAcademicProgramIntakeLevelAndStatus(id, status),
+    );
+  }
+  getAllByAcademicProgram(id: string) {
+    return useQuery(['schedules/program/:id', id], () =>
+      scheduleService.getAllByAcademicProgram(id),
+    );
+  }
+  getAllByAcademicProgramAndStatus(id: string, status: ScheduleStatus) {
+    return useQuery(['schedules/program/:id/:status', id, status], () =>
+      scheduleService.getAllByAcademicProgramAndStatus(id, status),
+    );
   }
 
   updateSchedule() {
