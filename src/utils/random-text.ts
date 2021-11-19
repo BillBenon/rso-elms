5da7fcec20d6f44ef6618e63b2cab406ad4b8a12
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2021-11-19 12:49:12 +0200

    refactor: remove . and $ from random text generator possibilities

--- src/utils/random-text.ts
+++ src/utils/random-text.ts
@@ -1,6 +1,6 @@
 export function randomString(length: number): string {
   var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUV.WXYZ0123456789abcdefghij$klmnopqrstuvwxyz';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
