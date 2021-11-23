980de269ae7a0c13cedefc1b7e6ff0cf3234d0f1
hirwablessing <hirwablessing7@gmail.com>
2021-11-23 21:56:30 +0200

    refactor: evaluation module

--- src/components/Organisms/forms/evaluation/EvaluationQuestionComponent.tsx
+++ src/components/Organisms/forms/evaluation/EvaluationQuestionComponent.tsx
@@ -6,13 +6,14 @@ import { ValueType } from '../../../../types';
 import {
   ICreateEvaluationQuestions,
   IEvaluationProps,
-  IEvaluationQuestion,
+  IMultipleChoice,
   IQuestionType,
 } from '../../../../types/services/evaluation.types';
 import { getLocalStorageData } from '../../../../utils/getLocalStorageItem';
 import Button from '../../../Atoms/custom/Button';
 import Icon from '../../../Atoms/custom/Icon';
 import Heading from '../../../Atoms/Text/Heading';
+import ILabel from '../../../Atoms/Text/ILabel';
 import DropdownMolecule from '../../../Molecules/input/DropdownMolecule';
 import InputMolecule from '../../../Molecules/input/InputMolecule';
 import TextAreaMolecule from '../../../Molecules/input/TextAreaMolecule';
@@ -21,10 +22,16 @@ export default function EvaluationQuestionComponent({
   handleGoBack,
   handleNext,
 }: IEvaluationProps) {
+  const multipleChoiceContent: IMultipleChoice = {
+    answer_content: '',
+    correct: false,
+  };
+
   const initialState: ICreateEvaluationQuestions = {
     evaluation_id: getLocalStorageData('evaluationId'),
     mark: 1,
     parent_question_id: '',
+    choices: [multipleChoiceContent],
     question: '',
     question_type: IQuestionType.OPEN,
     sub_questions: [],
@@ -38,11 +45,35 @@ export default function EvaluationQuestionComponent({
     setQuestions([...questions, newQuestion]);
   }
 
+  function handleAddMultipleMultipleChoiceAnswer(index: number) {
+    let choices = questions[index];
+    choices.choices.push(multipleChoiceContent);
+    setQuestions([...questions, choices]);
+  }
+
   function handleChange(index: number, { name, value }: ValueType) {
     let questionInfo = [...questions];
     questionInfo[index] = { ...questionInfo[index], [name]: value };
     setQuestions(questionInfo);
   }
+
+  function handleChoiceChange(
+    questionIndex: number,
+    choiceIndex: number,
+    { name, value }: ValueType,
+  ) {
+    let questionsClone = [...questions];
+    let question = questionsClone[questionIndex];
+
+    let choiceToUpdate = question.choices[choiceIndex];
+    choiceToUpdate = { ...choiceToUpdate, [name]: value };
+    question.choices[choiceIndex] = choiceToUpdate;
+    questionsClone[questionIndex] = question;
+
+    setQuestions(questionsClone);
+  }
+
+  console.log(questions);
 
   const { mutate } = evaluationStore.createEvaluationQuestions();
 
@@ -68,13 +99,13 @@ export default function EvaluationQuestionComponent({
   return (
     <>
       {questions.length > 0 ? (
-        questions.map((question: IEvaluationQuestion, index: number) => (
+        questions.map((question, index: number) => (
           <>
             <div
               className="flex justify-between w-2/3 bg-main px-6 py-10 mt-8"
               key={index}>
               <form
-                className="flex flex-col gap-4"
+                className="flex flex-col"
                 onSubmit={(e: FormEvent) => submitForm(index, e)}>
                 <DropdownMolecule
                   disabled={question.submitted}
@@ -88,7 +119,6 @@ export default function EvaluationQuestionComponent({
                   ]}>
                   Question type
                 </DropdownMolecule>
-
                 <TextAreaMolecule
                   readOnly={question.submitted}
                   name={'question'}
@@ -97,7 +127,38 @@ export default function EvaluationQuestionComponent({
                   handleChange={(e: ValueType) => handleChange(index, e)}>
                   Question {index + 1}
                 </TextAreaMolecule>
-
+                {/* multiple choice answers here */}
+                {question.choices.map((multipleQuestion, choiceIndex) => (
+                  <>
+                    <TextAreaMolecule
+                      key={multipleQuestion.answer_content}
+                      readOnly={question.submitted}
+                      name={'choices'}
+                      value={multipleQuestion.answer_content}
+                      placeholder="Enter question"
+                      handleChange={(e: ValueType) =>
+                        handleChoiceChange(index, choiceIndex, e)
+                      }>
+                      Answer choice {choiceIndex + 1}
+                    </TextAreaMolecule>{' '}
+                  </>
+                ))}
+                <div className="-mt-4 mb-1">
+                  <Button
+                    className="flex items-center pl-0"
+                    styleType="text"
+                    onClick={() => handleAddMultipleMultipleChoiceAnswer(index)}>
+                    <Icon
+                      name="add"
+                      size={17}
+                      useheightandpadding={false}
+                      stroke="primary"
+                    />
+                    <ILabel size="sm" className="cursor-pointer">
+                      Add answer
+                    </ILabel>
+                  </Button>
+                </div>
                 <InputMolecule
                   readonly={question.submitted}
                   type="number"
@@ -119,14 +180,14 @@ export default function EvaluationQuestionComponent({
                 </div>
               </form>
 
-              <div className="pr-14">
+              {/* <div className="pr-14">
                 <div className="flex items-center">
                   <Icon name="attach" size={17} fill="primary" />
                   <Heading color="primary" fontSize="base">
                     Attach file
                   </Heading>
                 </div>
-              </div>
+              </div> */}
             </div>
           </>
         ))
