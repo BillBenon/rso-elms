0b7c288a4fae104a0543371a0e241f839a93d456
hirwablessing <hirwablessing7@gmail.com>
2021-11-23 19:08:25 +0200

    chore: student can answer question

--- src/components/Atoms/custom/Icon.tsx
+++ src/components/Atoms/custom/Icon.tsx
@@ -7,7 +7,6 @@ import { Color, colorStyleType, IconType } from '../../../types';
 type IProps = {
   name: IconType;
   size?: number;
-  animate?: boolean;
   fill?: Color;
   stroke?: Color;
   transform?: string;
@@ -31,20 +30,6 @@ const StyledSVGIcon = styled(ReactSVG)`
         height: ${size}px;
       `}
 
-    ${({ animate }: IProps) =>
-      animate &&
-      css`
-        animation: spin 1s linear infinite;
-
-        @keyframes spin {
-          from {
-            transform: rotate(0deg);
-          }
-          to {
-            transform: rotate(360deg);
-          }
-        }
-      `}
     ${({ transform }: IProps) =>
       transform &&
       css`
@@ -70,7 +55,6 @@ export default function Icon({
   fill = 'none',
   stroke = 'none',
   transform = '',
-  animate = false,
   size = 24,
   bgColor,
   className = '',
@@ -84,7 +68,6 @@ export default function Icon({
       <StyledSVGIcon
         src={`/icons/${name}.svg`}
         stroke={stroke}
-        animate={animate}
         transform={transform}
         fill={fill}
         size={size}
