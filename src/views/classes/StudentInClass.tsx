7c72805998fcb29f78063e75b632e5035396006b
loraine <mukezwa@gmail.com>
2021-11-25 16:43:31 -0700

    fix: fixed build error

--- src/views/classes/StudentInClass.tsx
+++ src/views/classes/StudentInClass.tsx
@@ -33,7 +33,7 @@ function StudentInClass({ classId, label }: IStudentClass) {
         username: stud.student.user.username,
         'full name': stud.student.user.first_name + ' ' + stud.student.user.last_name,
         email: stud.student.user.email,
-        NID: stud.student.user.person.nid,
+        'ID Card': stud.student.user.person.nid,
         academy: stud.student.user.academy.name,
         status: stud.student.user.generic_status,
         user_type: stud.student.user.user_type,
