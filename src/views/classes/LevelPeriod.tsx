2531d0b68536a4dd93351d565007aa1d1a8aaa10
loraine <mukezwa@gmail.com>
2021-12-22 18:14:25 -0700

    refactor: show class actions on level periods

--- src/views/classes/LevelPeriod.tsx
+++ src/views/classes/LevelPeriod.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch, useHistory, useParams, useRouteMatch } from 'react-router-dom';
 
+import Button from '../../components/Atoms/custom/Button';
+import Heading from '../../components/Atoms/Text/Heading';
 import NoDataAvailable from '../../components/Molecules/cards/NoDataAvailable';
 import { Tab, Tabs } from '../../components/Molecules/tabs/tabs';
 import intakeProgramStore from '../../store/administration/intake-program.store';
@@ -36,9 +38,38 @@ function LevelPeriod() {
                     description="There are no periods assigned to this level, click on the below button to add them!"
                   />
                 ) : (
-                  <Tabs>
+                  <Tabs
+                    headerComponent={
+                      <div className="space-x-4">
+                        <Button
+                          styleType="outline"
+                          onClick={() =>
+                            history.push(
+                              `/dashboard/intakes/programs/${intakeId}/${id}/${intakeProg}/levels/${level}/view-class`,
+                            )
+                          }>
+                          View classes
+                        </Button>
+                        {prds.length !== 0 ? (
+                          <Button
+                            styleType="outline"
+                            onClick={() =>
+                              history.push(
+                                `/dashboard/intakes/programs/${intakeId}/${id}/${intakeProg}/levels/${level}/add-class`,
+                              )
+                            }>
+                            Add class
+                          </Button>
+                        ) : (
+                          <></>
+                        )}
+                      </div>
+                    }>
                     {prds.map((term) => (
                       <Tab key={term.id} label={term.academic_period.name}>
+                        <Heading fontWeight="semibold" className="py-2">
+                          Subjects
+                        </Heading>
                         <SubjectPeriod periodId={term.id + ''} />
                       </Tab>
                     ))}
