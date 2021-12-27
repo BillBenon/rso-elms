4ea22dd957296cc1ebe57268b5a8ce4cb9a59926
loraine <mukezwa@gmail.com>
2021-12-27 15:59:34 -0700

    fix: display institution information from db

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import ExperienceDetails from './components/Organisms/forms/auth/signup/experience/ExperienceDetails';
 import MoreInfo from './components/Organisms/forms/auth/signup/more-details/MoreInfo';
--- src/views/auth/Signin.tsx
+++ src/views/auth/Signin.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router';
 
-import Loader from '../../components/Atoms/custom/Loader';
 import AcademyProfileCard from '../../components/Molecules/cards/AcademyProfileCard';
 import NoDataAvailable from '../../components/Molecules/cards/NoDataAvailable';
 import SignInForm from '../../components/Organisms/forms/auth/signin/SignInForm';
@@ -9,15 +8,15 @@ import SignInWithSearch from '../../components/Organisms/forms/auth/signin/SignI
 import { institutionStore } from '../../store/administration/institution.store';
 
 function SignIn() {
-  const [openRegistration] = useState([]);
   const { path } = useRouteMatch();
-  const { data: institution, isLoading } = institutionStore.getAll();
+  const institution = institutionStore.getAll();
 
   return (
     <>
-      {isLoading ? (
+      {/* {institution.isLoading ? (
         <Loader />
-      ) : institution?.data.data.length === 0 ? (
+      ) :  */}
+      {institution.data?.data.data.length === 0 ? (
         <NoDataAvailable
           icon="academy"
           title={'No institution available'}
@@ -27,18 +26,9 @@ function SignIn() {
         />
       ) : (
         <div className="grid lg:grid-cols-2 h-screen grid-cols-1 bg-main">
-          {openRegistration.length ? (
-            <div className="hidden lg:block bg-secondary h-screen overflow-auto">
-              {/* <SignInWithRegControl
-              data={regData}
-              handleClick={(regData) => setShowData(regData)}
-            /> */}
-            </div>
-          ) : (
-            <div className="items-center justify-center hidden lg:flex bg-secondary">
-              <img className="block w-5/6" src={'/icons/login.svg'} alt="img" />
-            </div>
-          )}
+          <div className="items-center justify-center hidden lg:flex bg-secondary">
+            <img className="block w-5/6" src={'/icons/login.svg'} alt="img" />
+          </div>
           <div className="px-5 py-8 md:rounded-md mx-auto lg:py-20 h-screen">
             <AcademyProfileCard
               src="/images/nisslogo.png"
@@ -48,8 +38,8 @@ function SignIn() {
               txtSize="lg"
               fontWeight="semibold"
               color="primary"
-              subtitle={institution?.data.data[0].moto}>
-              {institution?.data.data[0].name}
+              subtitle={institution.data?.data.data[0].moto}>
+              {institution.data?.data.data[0].name}
             </AcademyProfileCard>
             <Switch>
               <Route exact path={`${path}`} render={() => <SignInForm />} />
