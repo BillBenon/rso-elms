daa512905be4ea81e61c238819d8906bbdb83d13
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2021-12-27 09:27:22 +0200

    feat: add loading indicators to program level class - schedule

--- src/views/schedule/ProgramsLevelClases.tsx
+++ src/views/schedule/ProgramsLevelClases.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import Button from '../../components/Atoms/custom/Button';
+import Loader from '../../components/Atoms/custom/Loader';
 import SkeletonLoader from '../../components/Atoms/custom/SkeletonLoader';
 import Heading from '../../components/Atoms/Text/Heading';
 import NoDataAvailable from '../../components/Molecules/cards/NoDataAvailable';
@@ -19,10 +20,10 @@ interface ParamType {
 export default function PrograsmLevelClasses() {
   const { intakeProgramId } = useParams<ParamType>();
 
-  const levelsInfo =
-    intakeProgramStore.getLevelsByIntakeProgram(intakeProgramId).data?.data.data || [];
+  const { data: levels, isLoading: levelsLoading } =
+    intakeProgramStore.getLevelsByIntakeProgram(intakeProgramId); //.data?.data.data || [];
 
-  let programInfo = levelsInfo[0]?.intake_program.program || undefined;
+  let programInfo = levels?.data.data[0]?.intake_program.program || undefined;
   const { data, isLoading } = classStore.getAllClasses();
 
   function filterClassByLevel(levelId: string | number) {
@@ -33,73 +34,79 @@ export default function PrograsmLevelClasses() {
 
   return (
     <div>
-      <TableHeader showBadge={false} title={programInfo?.name}>
+      <TableHeader showBadge={false} title={programInfo?.name || 'loading...'}>
         <Link to={`/dashboard/schedule/calendar/${programInfo?.id}`}>
           <Button styleType="outline">Program calendar</Button>
         </Link>
       </TableHeader>
-      {/* @ts-ignore */}
-      <Tabs>
-        {levelsInfo?.map((lvl) => (
-          <Tab
-            key={lvl.id}
-            className="py-3"
-            label={lvl.academic_program_level.level.name}>
-            <>
-              <Link
-                to={`/dashboard/schedule/calendar/${programInfo?.id}?in_level_id=${lvl.id}`}>
-                <span className="text-primary-500 font-medium block text-right">
-                  View calendar for this level
-                </span>
-              </Link>
-              <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6 mt-5">
-                {isLoading ? (
-                  [1, 2].map((i) => <SkeletonLoader height={200} key={i} />)
-                ) : filterClassByLevel(lvl.id).length === 0 ? (
-                  <div className="col-span-4">
-                    <NoDataAvailable
-                      title={'No classes registered here.'}
-                      description={'No classes registered in this intake level.'}
-                      showButton={false}
-                    />
-                  </div>
-                ) : (
-                  filterClassByLevel(lvl.id).map((cl) => (
-                    <div
-                      key={cl.id}
-                      className="bg-main rounded-md p-4 border-1 border-transparent hover:border-primary-500 cursor-pointer">
-                      <Heading fontSize="sm" color="txt-secondary" fontWeight="semibold">
-                        {cl.class_group_type}
-                      </Heading>
-                      <Heading className="pt-6" fontSize="sm" fontWeight="bold">
-                        {cl.class_name}
-                      </Heading>
-                      <div className="py-2 mt-3">
-                        <Link
-                          className="outline-none"
-                          to={`/dashboard/schedule/calendar/${programInfo?.id}?class_id=${cl.id}`}>
-                          <span className="text-primary-500 text-sm font-medium">
-                            View Calendar
-                          </span>
-                        </Link>
-                      </div>
-                      <div className="">
-                        <Link
-                          className="outline-none"
-                          to={`/dashboard/schedule/timetable/${cl.id}`}>
-                          <span className="text-primary-500 text-sm font-medium">
-                            View Time table
-                          </span>
-                        </Link>
-                      </div>
+      {levelsLoading ? (
+        <Loader />
+      ) : (
+        <Tabs>
+          {(levels?.data.data || [])?.map((lvl) => (
+            <Tab
+              key={lvl.id}
+              className="py-3"
+              label={lvl.academic_program_level.level.name}>
+              <>
+                <Link
+                  to={`/dashboard/schedule/calendar/${programInfo?.id}?in_level_id=${lvl.id}`}>
+                  <span className="text-primary-500 font-medium block text-right">
+                    View calendar for this level
+                  </span>
+                </Link>
+                <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6 mt-5">
+                  {isLoading ? (
+                    [1, 2].map((i) => <SkeletonLoader height={200} key={i} />)
+                  ) : filterClassByLevel(lvl.id).length === 0 ? (
+                    <div className="col-span-4">
+                      <NoDataAvailable
+                        title={'No classes registered here.'}
+                        description={'No classes registered in this intake level.'}
+                        showButton={false}
+                      />
                     </div>
-                  ))
-                )}
-              </div>
-            </>
-          </Tab>
-        ))}
-      </Tabs>
+                  ) : (
+                    filterClassByLevel(lvl.id).map((cl) => (
+                      <div
+                        key={cl.id}
+                        className="bg-main rounded-md p-4 border-1 border-transparent hover:border-primary-500 cursor-pointer">
+                        <Heading
+                          fontSize="sm"
+                          color="txt-secondary"
+                          fontWeight="semibold">
+                          {cl.class_group_type}
+                        </Heading>
+                        <Heading className="pt-6" fontSize="sm" fontWeight="bold">
+                          {cl.class_name}
+                        </Heading>
+                        <div className="py-2 mt-3">
+                          <Link
+                            className="outline-none"
+                            to={`/dashboard/schedule/calendar/${programInfo?.id}?class_id=${cl.id}`}>
+                            <span className="text-primary-500 text-sm font-medium">
+                              View Calendar
+                            </span>
+                          </Link>
+                        </div>
+                        <div className="">
+                          <Link
+                            className="outline-none"
+                            to={`/dashboard/schedule/timetable/${cl.id}`}>
+                            <span className="text-primary-500 text-sm font-medium">
+                              View Time table
+                            </span>
+                          </Link>
+                        </div>
+                      </div>
+                    ))
+                  )}
+                </div>
+              </>
+            </Tab>
+          ))}
+        </Tabs>
+      )}
     </div>
   );
 }
