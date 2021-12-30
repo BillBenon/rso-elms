215b03d23ea44d2a73277a2cc9019c5067d8cb43
Blessing Hirwa <hirwablessing7@gmail.com>
2021-12-30 14:16:48 +0000

    Update EvaluationSettings.tsx

--- src/components/Organisms/forms/evaluation/EvaluationSettings.tsx
+++ src/components/Organisms/forms/evaluation/EvaluationSettings.tsx
@@ -29,14 +29,13 @@ export default function EvaluationSettings({
 }: IEvaluationProps) {
   const authUser = authenticatorStore.authUser().data?.data.data;
 
-
-  const { data:inCharge, isLoading, refetch } =usersStore.getUsersByAcademyAndUserType(
+  const { data: inCharge } = usersStore.getUsersByAcademyAndUserType(
     authUser?.academy.id.toString() || '',
     UserType.INSTRUCTOR,
-    { page: 0, pageSize:1000, sortyBy: 'username' },
+    { page: 0, pageSize: 1000, sortyBy: 'username' },
   );
 
-  const instructors = inCharge?.data.data.content
+  const instructors = inCharge?.data.data.content;
 
   const [settings, setSettings] = useState<IEvaluationApproval>({
     approver: '',
@@ -143,27 +142,27 @@ export default function EvaluationSettings({
           True
         </SwitchMolecule>
       </div> */}
-      // <div className="pt-6">
-      //   <DropdownMolecule
-      //     width="60"
-      //     placeholder="marker"
-      //     options={getDropDownOptions({
-      //       inputs: instructors || [],
-      //       labelName: ['first_name', 'last_name'],
-      //     })}
-      //     name="marker"
-      //     handleChange={handleChange}>
-      //     To be marked by
-      //   </DropdownMolecule>
-      // </div>
-      {/* <div className="flex flex-col"> */}
-      <Button styleType="text" color="gray" className="mt-6" onClick={handleGoBack}>
-        Back
-      </Button>
-      <div className="pt-4">
-        <Button type="submit">Finish</Button>
+      {/* <div className="pt-6">
+         <DropdownMolecule
+          width="60"
+          placeholder="marker"
+          options={getDropDownOptions({
+            inputs: instructors || [],
+            labelName: ['first_name', 'last_name'],
+          })}
+          name="marker"
+          handleChange={handleChange}>
+          To be marked by
+        </DropdownMolecule>
+      </div> */}
+      <div className="flex flex-col">
+        <Button styleType="text" color="gray" className="mt-6" onClick={handleGoBack}>
+          Back
+        </Button>
+        <div className="pt-4">
+          <Button type="submit">Finish</Button>
+        </div>
       </div>
-      {/* </div> */}
       {/* <SwitchMolecule
         loading={false}
         name="shuffle"
