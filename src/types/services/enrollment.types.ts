3de66cb5bc3368000494e80537c2a46737accc38
loraine <mukezwa@gmail.com>
2021-12-30 18:34:24 -0700

    refactor: change enrollment level type

--- src/types/services/enrollment.types.ts
+++ src/types/services/enrollment.types.ts
@@ -1,12 +1,10 @@
 import { Table } from '..';
-import { IAcademicYearInfo } from './academicyears.types';
 import { InstructorProgram } from './instructor.types';
 import {
   LevelIntakeProgram,
   PromotionStatus,
   StudentIntakeProgram,
 } from './intake-program.types';
-import { AcademicProgramLevel } from './program.types';
 /* eslint-disable no-unused-vars */
 export enum EnrollmentStatus {
   PENDING,
@@ -84,7 +82,7 @@ export interface EnrollInstructorToModuleInfo extends Table, EnrollInstructorToM
 
 export interface EnrollInstructorLevelInfo extends Table {
   intake_program_instructor: InstructorProgram;
-  academic_year_program_level: LevelIntakeProgram;
+  academic_year_program_intake_level: LevelIntakeProgram;
   // academic_program_level: AcademicProgramLevel;
   // academic_year: IAcademicYearInfo;
 }
