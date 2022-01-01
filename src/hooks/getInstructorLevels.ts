2771915f4f27b07949d07431f832a8d18dec6f37
hirwablessing <hirwablessing7@gmail.com>
2022-01-01 17:11:56 +0200

    feat: create get instructor levels custom hook

---
+++ src/hooks/getInstructorLevels.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import enrollmentStore from '../store/administration/enrollment.store';
+import intakeProgramStore from '../store/administration/intake-program.store';
+import { LevelIntakeProgram } from '../types/services/intake-program.types';
+
+export default function useInstructorLevels(instructorId: string, intakeProgId: string) {
+  const [levels, setLevels] = useState<LevelIntakeProgram[]>([]);
+  const { data: getLevels } = intakeProgramStore.getLevelsByIntakeProgram(intakeProgId);
+
+  const { data: instructorLevels } = enrollmentStore.getInstructorLevels(instructorId);
+
+  let instructorLevelsIds = instructorLevels?.data.data.map(
+    (instLvl) => instLvl.academic_year_program_intake_level.id,
+  );
+
+  useEffect(() => {
+    setLevels(
+      getLevels?.data.data.filter((inst) => instructorLevelsIds?.includes(inst.id)) || [],
+    );
+  }, [getLevels?.data.data]);
+
+  return levels;
+}
