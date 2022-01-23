1f6b09103d9d29a83a09c3ebf1db26b1e1dfc525
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2022-01-23 15:23:23 +0200

    feat: integrate student calendar

--- src/views/schedule/StudentCalendar.tsx
+++ src/views/schedule/StudentCalendar.tsx
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 
 import Heading from '../../components/Atoms/Text/Heading';
 import CalendarComponent from '../../components/Organisms/schedule/calendar/CalendarComponent';
+import useAuthenticator from '../../hooks/useAuthenticator';
+import { getStudentShipByUserId } from '../../store/administration/intake-program.store';
+import { scheduleStore } from '../../store/timetable/calendar.store';
 import { BigCalendarEvent, DateRange } from '../../types/services/schedule.types';
+import { formatCalendarEvents } from '../../utils/calendar';
 import { getWeekBorderDays } from '../../utils/date-helper';
 
 export default function StudentCalendar() {
@@ -11,12 +15,21 @@ export default function StudentCalendar() {
     end_date: getWeekBorderDays().sunday,
   });
 
+  const { user } = useAuthenticator();
+  const student = getStudentShipByUserId(user?.id.toString() || '', !!user?.id).data?.data
+    .data[0];
+
+  const { data, refetch } = scheduleStore.getAllByStudent(
+    student?.id.toString(),
+    dateRange,
+  );
+
+  const events: BigCalendarEvent[] = formatCalendarEvents(data?.data.data || []);
+
   const handleApplyDateRange = (r: DateRange) => {
     setdateRange(r);
-    // refetch();
+    refetch();
   };
-
-  const events: BigCalendarEvent[] = [];
 
   return (
     <div>
