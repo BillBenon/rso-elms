0f0924f98130b95aafd26d682099c3dd6be13258
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2022-02-28 23:31:21 +0200

    feat: embed dynamic data in chart

--- src/components/Organisms/chart/Barchart.tsx
+++ src/components/Organisms/chart/Barchart.tsx
@@ -10,52 +10,19 @@ import {
   YAxis,
 } from 'recharts';
 
-const data = [
-  {
-    name: 'Department A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Department B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Department C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Department D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Department E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Department F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Department G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+interface IBarchatProps {
+  fill?: string;
+  xAxisLabel: string;
+  dataKey: string;
+  data: any[];
+}
 
-export default function Barchart() {
+export default function Barchart({
+  data,
+  xAxisLabel,
+  dataKey,
+  fill = '#8884d8',
+}: IBarchatProps) {
   return (
     <div className="pt-6">
       <ResponsiveContainer width="100%" height={500}>
@@ -68,12 +35,11 @@ export default function Barchart() {
             bottom: 5,
           }}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey={xAxisLabel} />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+          <Bar dataKey={dataKey} maxBarSize={50} fill={fill} />
         </BarChart>
       </ResponsiveContainer>
     </div>
