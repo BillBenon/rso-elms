21b6c86da085d658d146f2f454abd48250e44b7f
Blessing Hirwa <hirwablessing7@gmail.com>
2022-02-28 13:39:07 +0200

    fix: show evaluations to only people with manage evaluations privilege, refactor some components according to privileges

--- src/hooks/usePrivilege.ts
+++ src/hooks/usePrivilege.ts
@@ -2,7 +2,7 @@ import { Privileges } from '../types';
 import cookie from '../utils/cookie';
 import useAuthenticator from './useAuthenticator';
 
-export default function usePrivilege(privilege: Privileges) {
+export function usePrivilege(privilege: Privileges) {
   const { user } = useAuthenticator();
   const user_role_cookie = cookie.getCookie('user_role') || '';
   const user_role = user?.user_roles?.find((role) => role.id + '' === user_role_cookie);
