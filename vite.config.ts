3b174601be29bd116338fab84882f1c2b4ba38ab
claranceliberi <liberintwari@gmail.com>
2022-03-02 12:18:25 +0200

    chore: fix charset issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove();
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
 });
