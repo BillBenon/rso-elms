e787979c2f107a51db390b445b6107f9c0a47c6f
claranceliberi <liberintwari@gmail.com>
2022-03-02 12:24:49 +0200

    chore: increase chunk size limit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
   css: {
     postcss: {
       plugins: [
