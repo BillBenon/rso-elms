59a6f44f5990ea71891a1150ca3485734694c6f7
claranceliberi <liberintwari@gmail.com>
2022-04-05 13:11:18 +0200

    fix: infinite load

--- src/hooks/useAuthenticator.ts
+++ src/hooks/useAuthenticator.ts
@@ -1,18 +1,17 @@
 import { AxiosError } from 'axios';
-import { create, isSet } from 'lodash';
 import { FormEvent, useCallback, useContext, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useHistory } from 'react-router-dom';
 
+import UserContext from '../context/UserContext';
 import { queryClient } from '../plugins/react-query';
 import { authenticatorStore } from '../store/administration';
 import { LoginInfo, Response } from '../types';
-import { AuthUser } from '../types/services/user.types';
 import cookie from '../utils/cookie';
 let created = false;
 
 export default function useAuthenticator() {
-  const [user, setUser] = useState<AuthUser>();
+  const { user, setUser } = useContext(UserContext);
   const [isUserLoading, setIsUserLoading] = useState(false);
   const [_isError, setIsError] = useState(false);
   const [_error, setError] = useState<AxiosError<Response>>();
@@ -21,7 +20,6 @@ export default function useAuthenticator() {
   const { mutateAsync } = authenticatorStore.login();
   const [isLoggingIn, setIsLoggingIn] = useState(false);
   const history = useHistory();
-  const [isSettingUser, setIsSettingUser] = useState(false);
 
   const fetchData = useCallback(async () => {
     setIsUserLoading(true);
@@ -30,21 +28,16 @@ export default function useAuthenticator() {
       user?.id,
       data?.data.data.id,
       data?.data.data.id != user?.id,
-      isSettingUser,
       isError,
       isError,
       data,
       isUserLoading,
     );
 
-    const context = useContext();
-    console.log(context, 'context');
-
-    if (isSuccess && data?.data.data.id != user?.id && !isSettingUser) {
+    if (isSuccess && data?.data.data.id != user?.id) {
       console.log('confirmations');
       setUser((_old) => {
         console.log('trapish', data?.data.data);
-        setIsSettingUser(true);
         return data?.data.data;
       });
       setUserAvailable(true);
@@ -54,20 +47,18 @@ export default function useAuthenticator() {
 
     setIsError(isError);
     setIsUserLoading(false);
-  }, [isSettingUser, isUserLoading, refetch, user?.id]);
+  }, [isUserLoading, refetch, setUser, user?.id]);
 
   useEffect(() => {
     if (!created) {
       fetchData();
       created = true;
     }
-    console.log('authenticator triggered , useEffect', user);
+    // console.log('authenticator triggered , useEffect');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    console.log(user, 'user has changed');
-  }, [user]);
+  useEffect(() => {}, [user]);
 
   async function login<T>(e: FormEvent<T>, details: LoginInfo) {
     e.preventDefault();
