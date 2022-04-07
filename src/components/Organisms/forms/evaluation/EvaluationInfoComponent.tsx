bc4da2a2980cc16da6c67142cab25e0c327a7edc
didiermun <didiermunezer38@gmail.com>
2022-04-07 14:12:30 +0200

    Merge branch 'staging' of http://197.243.110.146:9999/e-learning/frontend-service into healer-fixes

--- src/components/Organisms/forms/evaluation/EvaluationInfoComponent.tsx
+++ src/components/Organisms/forms/evaluation/EvaluationInfoComponent.tsx
@@ -3,6 +3,7 @@ import moment from 'moment';
 import React, { FormEvent, useEffect, useMemo, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useLocation } from 'react-router-dom';
+
 import useAuthenticator from '../../../../hooks/useAuthenticator';
 import usePickedRole from '../../../../hooks/usePickedRole';
 import { enrollmentService } from '../../../../services/administration/enrollments.service';
