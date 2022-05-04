2c8a042023de17af40f4aebfc2e9f2aae264b5e7
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-05-04 18:47:07 +0200

    chore: fix submission type

--- src/components/Organisms/forms/evaluation/EvaluationInfoComponent.tsx
+++ src/components/Organisms/forms/evaluation/EvaluationInfoComponent.tsx
@@ -4,7 +4,6 @@ import React, { FormEvent, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 import { useHistory, useLocation } from 'react-router-dom';
-
 import useAuthenticator from '../../../../hooks/useAuthenticator';
 import usePickedRole from '../../../../hooks/usePickedRole';
 import { enrollmentService } from '../../../../services/administration/enrollments.service';
@@ -31,11 +30,11 @@ import {
   IEvaluationTypeEnum,
   IMarkingType,
   IQuestionaireTypeEnum,
-  ISubmissionTypeEnum,
+  ISubmissionTypeEnum
 } from '../../../../types/services/evaluation.types';
 import {
   getDropDownOptions,
-  getDropDownStatusOptions,
+  getDropDownStatusOptions
 } from '../../../../utils/getOption';
 import Button from '../../../Atoms/custom/Button';
 import Heading from '../../../Atoms/Text/Heading';
@@ -47,6 +46,7 @@ import InputMolecule from '../../../Molecules/input/InputMolecule';
 import MultiselectMolecule from '../../../Molecules/input/MultiselectMolecule';
 import RadioMolecule from '../../../Molecules/input/RadioMolecule';
 import SelectMolecule from '../../../Molecules/input/SelectMolecule';
+
 
 const initialState: IEvaluationSectionBased = {
   evaluation_id: '',
@@ -670,7 +670,7 @@ export default function EvaluationInfoComponent() {
                   /*@ts-ignore */
                   value={details?.content_format}
                   width="64"
-                  name="submision_type"
+                  name="content_formats"
                   placeholder="Select submission type"
                   handleChange={handleChange}
                   options={[
