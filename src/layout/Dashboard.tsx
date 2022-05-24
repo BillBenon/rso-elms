940b2c0ef39df079e4f7e3a398aba7f55237b99f
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-05-24 19:17:26 +0200

    fix: show one navbar

--- src/layout/Dashboard.tsx
+++ src/layout/Dashboard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Navbar from '../components/Molecules/navigation/Navbar';
 import Navigation from '../components/Molecules/navigation/Navigation';
 import Sidebar from '../components/Molecules/sidebar/Sidebar';
 
@@ -8,9 +7,7 @@ type IDashboard = { children: React.ReactNode };
 
 export default function Dashboard({ children }: IDashboard) {
   return (
-    <>
-      <Navbar />
-      <div className="border-b-4 border-red-600 my-10" />
+    <div>
       <Navigation hasProfile={true} hasChangePassword={true} hasSetting={true} />
       <div className="flex bg-secondary">
         <div className="hidden md:block w-80 h-screen top-0 lg:sticky ">
@@ -21,6 +18,6 @@ export default function Dashboard({ children }: IDashboard) {
           <div className="block relative w-full h-auto py-5 px-8">{children}</div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
