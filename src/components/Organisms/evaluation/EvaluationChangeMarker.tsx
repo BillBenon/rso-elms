f07078217cc8e65116b606f631e0eb651d5b6376
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-06-08 15:22:55 +0200

    fix: manual marking

--- src/components/Organisms/evaluation/EvaluationChangeMarker.tsx
+++ src/components/Organisms/evaluation/EvaluationChangeMarker.tsx
@@ -3,20 +3,15 @@ import toast from 'react-hot-toast';
 
 import { evaluationStore } from '../../../store/evaluation/evaluation.store';
 import { ValueType } from '../../../types';
-import {
-  IEvaluationInfo,
-  IEvaluationSectionBased,
-} from '../../../types/services/evaluation.types';
+import { IEvaluationSectionBased } from '../../../types/services/evaluation.types';
 import { UserInfo } from '../../../types/services/user.types';
 import { getDropDownOptions } from '../../../utils/getOption';
 import Button from '../../Atoms/custom/Button';
 import SelectMolecule from '../../Molecules/input/SelectMolecule';
 export function EvaluationChangeMarker({
-  evaluation,
   module,
   markers,
 }: {
-  evaluation: IEvaluationInfo;
   module: IEvaluationSectionBased;
   markers: UserInfo[];
 }) {
@@ -39,7 +34,12 @@ export function EvaluationChangeMarker({
           inputs: markers,
           labelName: ['first_name', 'last_name'],
           //@ts-ignore
-          getOptionLabel: (mark: UserInfo) => mark.first_name + ' ' + mark.last_name,
+          getOptionLabel: (mark: UserInfo) =>
+            mark.person?.current_rank.abbreviation +
+            ' ' +
+            mark.first_name +
+            ' ' +
+            mark.last_name,
         })}
         placeholder="marker"
       />
